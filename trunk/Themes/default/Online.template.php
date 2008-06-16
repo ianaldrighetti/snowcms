3106c476d6a7370b0a2eb186b00da9563cd2f6e5
nscnetworks <nscnetworks@f10a5a2d-b74f-0410-8a28-69e04d5e54c3>
2008-06-16 22:18:35 +0000

    Coded to near completion, missing the code inside the foreach

--- trunk/Themes/default/Online.template.php
+++ trunk/Themes/default/Online.template.php
@@ -5,5 +5,17 @@ if(!defined('Snow'))
   die("Hacking Attempt...");
 
 function Main() {
-global $cmsurl, $l, $user;
+global $cmsurl, $l, $settings, $user;
+echo '
+  <h1>'.$l['online_header'].'</h1>
+  <p>'.$l['online_desc'].'</p>
+  <table>
+    <tr>
+      <td>'.$l['online_user'].'</td>'; if(can('view_online_special')) { echo '<td>'.$l['online_ip'].'</td>'; } echo '<td>'.$l['online_currently_viewing'].'</td>
+    </tr>';
+  foreach($settings['page']['online'] as $online) {
+  
+  }
+  echo '
+  </table>';
 }
