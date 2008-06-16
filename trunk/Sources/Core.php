1383b22d820a8a6d1d2acf9fd5426a1e057c5d65
nscnetworks <nscnetworks@f10a5a2d-b74f-0410-8a28-69e04d5e54c3>
2008-06-16 22:42:40 +0000

    Added to the loadSettings function to load up all usernames

--- trunk/Sources/Core.php
+++ trunk/Sources/Core.php
@@ -19,6 +19,9 @@ global $db_prefix;
   $result = mysql_query("SELECT * FROM {$db_prefix}settings");
     while($row = mysql_fetch_assoc($result)) 
       $settings[$row['variable']] = $row['value'];
+  $result = mysql_query("SELECT `id`,`username`,`display_name` FROM {$db_prefix}members");
+    while($row = mysql_fetch_assoc($result)) 
+      $settings['users'][$row['id']] = $row['display_name'] ? $row['display_name'] : $row['username'];
   return $settings;
 } 
  
