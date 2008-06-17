1b683e97c26faa3972d85058919ecec35a1684ef
nscnetworks <nscnetworks@f10a5a2d-b74f-0410-8a28-69e04d5e54c3>
2008-06-17 23:22:49 +0000

    added somethings i can't remember

--- trunk/Languages/english.language.php
+++ trunk/Languages/english.language.php
@@ -56,6 +56,8 @@ $l['admin_cant_get_news_1'] = 'We were unable to get the latest SnowCMS News fro
 $l['admin_cant_get_news_2'] = 'Unable to get the latest news from <a href="http://www.snowcms.com">SnowCMS</a> due to a server timeout. Refresh the page, or check out our site';
 $l['admin_current_version'] = 'Your SnowCMS Version:';
 $l['admin_snowcms_current_version'] = 'Latest SnowCMS Version:';
+$l['admin_error_header'] = 'Error';
+$l['admin_error_reason'] = 'You don\'t have sufficient permission to access the Admin CP!';
 
 // Settings.template.php
 $l['basicsettings_title'] = 'Basic Settings';
