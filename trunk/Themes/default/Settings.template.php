51cfc90d3cf1fb8badf8fdefd0db4fb820007e35
nscnetworks <nscnetworks@f10a5a2d-b74f-0410-8a28-69e04d5e54c3>
2008-06-17 21:51:09 +0000

    Lists Basic Settings

--- trunk/Themes/default/Settings.template.php
+++ trunk/Themes/default/Settings.template.php
@@ -9,5 +9,18 @@ global $cmsurl, $settings, $l, $user;
 }
 
 function Basic() {
-
+global $cmsurl, $settings, $l, $user;
+  echo '
+  <h1>'.$l['basicsettings_header'].'</h1>
+  <p>'.$l['basicsettings_desc'].'</p>';
+  echo '
+  <table>';
+  foreach($settings['page']['settings'] as $setting) {
+    echo '
+      <tr>
+        <td>'.$l['basicsettings_'.$setting].'</td><td><input name="'.$setting.'" type="text" value="'.$settings[$setting].'"/></td>
+      </tr>';
+  }
+  echo '
+  </table>';
 }
