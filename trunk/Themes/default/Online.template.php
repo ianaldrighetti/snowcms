646cc200e19f030a4777a9ab58ab6ffecb1e3d55
nscnetworks <nscnetworks@f10a5a2d-b74f-0410-8a28-69e04d5e54c3>
2008-06-17 00:38:07 +0000

    Updated to now show time last active

--- trunk/Themes/default/Online.template.php
+++ trunk/Themes/default/Online.template.php
@@ -11,12 +11,12 @@ echo '
   <p>'.$l['online_desc'].'</p>
   <table>
     <tr>
-      <td>'.$l['online_user'].'</td>'; if(can('view_online_special')) { echo '<td>'.$l['online_ip'].'</td>'; } echo '<td>'.$l['online_currently_viewing'].'</td>
+      <td>'.$l['online_user'].'</td>'; if(can('view_online_special')) { echo '<td>'.$l['online_ip'].'</td>'; } echo '<td>'.$l['online_time'].'</td><td>'.$l['online_currently_viewing'].'</td>
     </tr>';
   foreach($settings['page']['online'] as $online) {
     echo '
     <tr>
-      <td>'; if($online['is_user']) { echo '<a href="'.$cmsurl.'index.php?action=profile&u='.$online['user_id'].'">'; } echo $online['user']; if($online['is_user']) { echo '</a>'; } echo '</td>'; if(can('view_online_special')) { echo '<td>'.$online['ip'].'</td>'; } echo '<td>'.$online['page'].'</td>
+      <td>'; if($online['is_user']) { echo '<a href="'.$cmsurl.'index.php?action=profile&u='.$online['user_id'].'">'; } echo $online['user']; if($online['is_user']) { echo '</a>'; } echo '</td>'; if(can('view_online_special')) { echo '<td>'.$online['ip'].'</td>'; } echo '<td>'.$online['time'].'</td><td>'.$online['page'].'</td>
     </tr>';
   }
   echo '
