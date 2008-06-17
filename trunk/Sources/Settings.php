415a934173d1834cebb6398e94afdf36708602a4
nscnetworks <nscnetworks@f10a5a2d-b74f-0410-8a28-69e04d5e54c3>
2008-06-17 22:01:20 +0000

    Added Sanitizing

--- trunk/Sources/Settings.php
+++ trunk/Sources/Settings.php
@@ -27,12 +27,13 @@ global $cmsurl, $db_prefix, $l, $settings, $user;
   if(!empty($_REQUEST['update'])) {
     $queries = array();
     foreach($basic as $setting) {
-      $queries[] = "UPDATE {$db_prefix}settings SET `value` = '{$_REQUEST['site_name']}' WHERE `variable` = '{$setting}' LIMIT 1";
+      $_REQUEST[$setting] = clean($_REQUEST[$setting]);
+      $query = "UPDATE {$db_prefix}settings SET `value` = '{$_REQUEST[$setting]}' WHERE `variable` = '{$setting}' LIMIT 1";
+      $result = mysql_query($query);
     }
-    $query = implode(",", $queries);
-    $result = mysql_query($query) or die(mysql_error());
     // Reload Settings D:
     $settings = loadSettings();
+    $settings['menu'] = loadMenus();
   }
   $settings['page']['title'] = $l['basicsettings_title'];
   $settings['page']['settings'] = $basic;
