9845dd587775d01c8562d44daf71ca16493dadcc
nscnetworks <nscnetworks@f10a5a2d-b74f-0410-8a28-69e04d5e54c3>
2008-06-17 09:51:45 +0000

    Displays current news better, and the works of your SnowCMS version, and the latest SnowCMS Version have been started to be built in

--- trunk/Themes/default/Admin.template.php
+++ trunk/Themes/default/Admin.template.php
@@ -8,5 +8,15 @@ global $cmsurl, $settings, $l, $user;
   echo '
   <div style="overflow: auto; width: 503px; height: 100px;">
     '.$settings['page']['news'].'
-  </div>';
+  </div>
+  <table>
+    <tr>
+      <td>'.$l['admin_current_version'].'</td>
+      <td>'.$settings['version'].'</td>
+    </tr>
+    <tr>
+      <td>'.$l['admin_snowcms_current_version'].'</td>
+      <td>v0.2</td>
+    </tr>
+  </table>';
 }
