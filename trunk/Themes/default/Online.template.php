aaa2cf468dfb1337d1cd70fc260c500edef07d5c
nscnetworks <nscnetworks@f10a5a2d-b74f-0410-8a28-69e04d5e54c3>
2008-06-17 00:29:29 +0000

    Added the code inside the foreach()

--- trunk/Themes/default/Online.template.php
+++ trunk/Themes/default/Online.template.php
@@ -14,7 +14,10 @@ echo '
       <td>'.$l['online_user'].'</td>'; if(can('view_online_special')) { echo '<td>'.$l['online_ip'].'</td>'; } echo '<td>'.$l['online_currently_viewing'].'</td>
     </tr>';
   foreach($settings['page']['online'] as $online) {
-  
+    echo '
+    <tr>
+      <td>'; if($online['is_user']) { echo '<a href="'.$cmsurl.'index.php?action=profile&u='.$online['user_id'].'">'; } echo $online['user']; if($online['is_user']) { echo '</a>'; } echo '</td>'; if(can('view_online_special')) { echo '<td>'.$online['ip'].'</td>'; } echo '<td>'.$online['page'].'</td>
+    </tr>';
   }
   echo '
   </table>';
