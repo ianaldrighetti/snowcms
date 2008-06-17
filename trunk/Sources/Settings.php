f9037befe88b52652ff2a69fa38301c7cf90ee86
nscnetworks <nscnetworks@f10a5a2d-b74f-0410-8a28-69e04d5e54c3>
2008-06-17 21:15:06 +0000

    Added BasicSettings function

--- trunk/Sources/Settings.php
+++ trunk/Sources/Settings.php
@@ -13,5 +13,27 @@
 
 if(!defined("Snow"))
   die("Hacking Attempt...");
-
+  
+function BasicSettings() {
+global $cmsurl, $db_prefix, $l, $settings, $user;
+  $basic = array(
+    'site_name',
+    'slogan',
+    'login_threshold',
+    'remember_time',
+    'timeformat'
+  );
+  if(!empty($_REQUEST['update'])) {
+    $queries = array();
+    foreach($basic as $setting) {
+      $queries[] = "UPDATE {$db_prefix}settings SET `value` = '{$_REQUEST['site_name']}' WHERE `variable` = '{$setting}' LIMIT 1";
+    }
+    $query = implode(",", $queries);
+    $result = mysql_query($query) or die(mysql_error());
+    // Reload Settings D:
+    $settings = loadSettings();
+  }
+  $settings['page']['title'] = $l['basicsettings_title'];
+  loadTheme('Settings','BasicSettings');
+}
 ?>
