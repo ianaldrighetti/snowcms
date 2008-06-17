03be2b89cbce50ba9cd4979086f0def6de5b97e5
nscnetworks <nscnetworks@f10a5a2d-b74f-0410-8a28-69e04d5e54c3>
2008-06-17 01:19:01 +0000

    Initially Added

---
+++ trunk/Sources/Profile.php
@@ -0,0 +1,20 @@
+<?php
+//                 SnowCMS
+//           By aldo and soren121
+//  Founded by soren121 & co-founded by aldo
+//    http://snowcms.northsalemcrew.net
+//
+// SnowCMS is released under the GPL v3 License
+// Which means you are free to edit it and then
+//       redistribute it as your wish!
+// 
+//                Profile.php file 
+
+
+if(!defined("Snow"))
+  die("Hacking Attempt...");
+
+function Profile() {
+global $cmsurl, $db_prefix, $l, $settings, $source_dir, $user;
+}
+?>
