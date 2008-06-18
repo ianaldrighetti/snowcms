3ee6b5f253d8a05b0b6a0375b922d7e39cdfe3d7
nscnetworks <nscnetworks@f10a5a2d-b74f-0410-8a28-69e04d5e54c3>
2008-06-18 02:05:19 +0000

    Added form to add pages, and messages to show if page was made successfully or if it failed

--- trunk/Themes/default/ManagePages.template.php
+++ trunk/Themes/default/ManagePages.template.php
@@ -6,5 +6,35 @@ if(!defined('Snow'))
 
 function Main() {
 global $cmsurl, $settings, $l, $user;
-
+  echo '
+  <h1>'.$l['managepages_header'].'</h1>
+  <p>'.$l['managepages_desc'].'</p>';
+  if($settings['page']['make_page']) {
+    if($settings['page']['make_page']['status']) {
+      echo '
+      <div id="page_success">
+        <p>'.$settings['page']['make_page']['info'].'</p>
+      </div>';
+    }
+    else {
+      echo '
+      <div id="page_fail">
+        <p>'.$settings['page']['make_page']['info'].'</p>
+      </div>';    
+    }
+  }
+  else {
+    echo '
+    <form action="" method="post">
+      <table>
+        <tr>
+          <td>'.$l['managepages_pagetitle'].'</td><td><input name="page_title" type="text" value=""/></td>
+        </tr>
+        <tr>
+          <td>&nbsp;</td><td><input name="make_page" type="submit" value="'.$l['managepages_makepage'].'"/></td>
+        </tr>
+      </table>
+    </form>';
+  }
 }
+?>
