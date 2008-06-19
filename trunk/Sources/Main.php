dc9be4565fbd5693bf29602f15e01fbc394adcd9
nscnetworks <nscnetworks@f10a5a2d-b74f-0410-8a28-69e04d5e54c3>
2008-06-19 06:56:13 +0000

    Added comments

--- trunk/Sources/Main.php
+++ trunk/Sources/Main.php
@@ -18,10 +18,13 @@ function Home() {
 global $cmsurl, $db_prefix, $l, $settings, $source_dir, $user;
   // So uh, What should we do? 1 = Show a Page, 2 = Show News
   if($settings['main_page']==1) {
+    // Include the Page.php Source File
     require_once($source_dir.'/Page.php');
+    // Call on Page(true); the true means load the Page that is set to be the Homes Default Page
     Page(true);
   }
   else {
+    // Get the News.php Source File, and show the News
     require_once($source_dir.'/News.php');
     News();
   }
