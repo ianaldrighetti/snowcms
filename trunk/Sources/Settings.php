f23d80c0b81232da957735cb7808827d45367bf9
nscnetworks <nscnetworks@f10a5a2d-b74f-0410-8a28-69e04d5e54c3>
2008-06-19 07:28:31 +0000

    Added comments

--- trunk/Sources/Settings.php
+++ trunk/Sources/Settings.php
@@ -24,17 +24,19 @@ global $cmsurl, $db_prefix, $l, $settings, $user;
     'remember_time',
     'timeformat'
   );
+  // Are we updating them?
   if(!empty($_REQUEST['update'])) {
-    $queries = array();
+    // Set them all!
     foreach($basic as $setting) {
       $_REQUEST[$setting] = clean($_REQUEST[$setting]);
       $query = "UPDATE {$db_prefix}settings SET `value` = '{$_REQUEST[$setting]}' WHERE `variable` = '{$setting}' LIMIT 1";
       $result = mysql_query($query);
     }
-    // Reload Settings D:
+    // Reload Settings D: Or they won't be the latest, menu's need to be reset too O.o
     $settings = loadSettings();
     $settings['menu'] = loadMenus();
   }
+  // Set title, pass on $basic, and load Settings template with the Basic function
   $settings['page']['title'] = $l['basicsettings_title'];
   $settings['page']['settings'] = $basic;
   loadTheme('Settings','Basic');
