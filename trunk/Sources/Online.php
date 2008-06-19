92a90f9d06628ae2f4967135ca466782171d4209
nscnetworks <nscnetworks@f10a5a2d-b74f-0410-8a28-69e04d5e54c3>
2008-06-19 07:03:00 +0000

    Added comments

--- trunk/Sources/Online.php
+++ trunk/Sources/Online.php
@@ -16,18 +16,24 @@ if(!defined("Snow"))
   
 function Online() {
 global $cmsurl, $db_prefix, $l, $settings, $user;
+  // Can they view the who is online?
   if(can('view_online')) {
+    // Set online as an array, just incase their is nothing in the DB, but if you view ?action=online, 1 should be there by now
     $online = array();
+    // Get them, Ordered by `last_active` Descending (Show the newest on top)
     $result = mysql_query("SELECT * FROM {$db_prefix}online ORDER BY `last_active` DESC");
       while($row = mysql_fetch_assoc($result)) {
-        $type = explode(":", $row['page']);
+        // Are they on a ?page= or ?action=
+        $type = @explode(":", $row['page']);
         if($type[0]=='action') {
+          // Get its title... Or say it is unknown
           if(isset($l[$type[1].'_title']))
             $page_title = $l[$type[1].'_title'];
           else
             $page_title = $l['online_title_unknown'];
         }
         elseif($type[0]=='page') {
+          // They are on a ?page=, so get its title, or say it is unknown
           if(isset($settings['page_titles'][$type[1]]))
             $page_title = $settings['page_title'][$type[1]];
           else
@@ -35,6 +41,7 @@ global $cmsurl, $db_prefix, $l, $settings, $user;
         }
         else
           $page_title = $l['online_title_unknown'];
+        // Add them to the $online array, give name, ID, page, ip, time last active
         $online[] = array(
           'user_id' => $row['user_id'],
           'is_user' => $row['user_id'] ? true : false,
@@ -44,11 +51,12 @@ global $cmsurl, $db_prefix, $l, $settings, $user;
           'time' => date("g:i:sA", $row['last_active'])
         );
       }
+    // Save the $online to $settings so we can pass it on, also set title and load template
     $settings['page']['online'] = $online;
     $settings['page']['title'] = $l['online_title'];
     loadTheme("Online");
   }
   else {
-  
+    // They can't view it, I haven't gotten this far yet... lol. I have been a bad Dev :P
   }
 }
