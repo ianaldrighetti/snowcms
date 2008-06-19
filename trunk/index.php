cb856cecf976111b44c908fe0a4a7af0392f50ec
nscnetworks <nscnetworks@f10a5a2d-b74f-0410-8a28-69e04d5e54c3>
2008-06-19 06:18:26 +0000

    Added comments

--- trunk/index.php
+++ trunk/index.php
@@ -16,34 +16,37 @@ define("Snow", true);
 
 // Load Some Important Files
 require_once('./config.php');
+// SnowCMS is not installed, so take them to the installer so they can install SnowCMS
 if(!$scms_installed)
   header("Location: install.php");
+// Load Core.php, this file has many things we need, such as the functions we will call on soon :)
 require_once($source_dir.'/Core.php');
 
+  // Connect to MySQL
   mysql_connect($mysql_host, $mysql_user, $mysql_passwd) or die(mysql_error());
-  mysql_select_db($mysql_db) or die(mysql_error());
 
 // Load up a few things :P Such as $settings, $l[anguage], and $user, the permissions too!
-$user = loadUser();
-$settings = loadSettings();
-$l = loadLanguage();
-$perms = loadPerms();
-$settings['menu'] = loadMenus();
-WriteOnline();
+  $user = loadUser();
+  $settings = loadSettings();
+  $l = loadLanguage();
+  $perms = loadPerms();
+  $settings['menu'] = loadMenus();
+// Write that this Guest/User is online
+  WriteOnline();
 
   // What should we do? ._.
   if((empty($_REQUEST['action'])) && (empty($_REQUEST['page']))) {
-    // Nothing, Get the Main Page!
+    // Neither the ?action= is set, nor the ?page= is set, so load Home()
     require_once($source_dir.'/Main.php');
       Home();
   }
   elseif(!empty($_REQUEST['page'])) {
-    // Page isn't empty! :D Lets load that Page!
+    // ?page= is not empty, load the page
     require_once($source_dir.'/Page.php');
       Page();
   }
   else {
-    // Action is not empty!
+    // ?action is not empty! load it
     // To add an action, it is Quite Simple, Simply add a row like this:
     // 'ACTION_NAME' => array('ACTION_SOURCE_FILE','FUNCTION_TO_CALL_ON_IN_FILE'),
     $actions = array(
@@ -57,6 +60,8 @@ WriteOnline();
       'register' => array('Register.php','Register'),
       'register2' => array('Register.php','Register2')
     );
+    // Is this action they are requesting even in the $actions array? If not, we don't want an error message.
+    // So, lets load the Home() Function
     if(!is_array(@$actions[$_REQUEST['action']])) {
       require_once($source_dir.'/Main.php');
         Home();  
