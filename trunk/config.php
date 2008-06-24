195deaf75cc71dd48ca127de859969a22eaa1cbc
nscnetworks <nscnetworks@f10a5a2d-b74f-0410-8a28-69e04d5e54c3>
2008-06-24 23:32:27 +0000

    remove stuff that shouldn't be there!

--- trunk/config.php
+++ trunk/config.php
@@ -15,18 +15,18 @@ if(!defined("Snow"))
   die("Hacking Attempt...");
   
 // Your MySQL Information
-$mysql_host = 'localhost'; # Your MySQL Host, doubt you will change this
-$mysql_user = 'admin'; # Your MySQL Username
-$mysql_passwd = '123456'; # Your MySQL Password
-$mysql_db = 'snow'; # Your MySQL DB
+$mysql_host = ''; # Your MySQL Host, doubt you will change this
+$mysql_user = ''; # Your MySQL Username
+$mysql_passwd = ''; # Your MySQL Password
+$mysql_db = ''; # Your MySQL DB
 $mysql_prefix = 'scms_'; # Prefix for your database
 
 // Some SnowCMS Specific Settings
-$source_dir = 'C:\\wamp\\www\\nSnow\\Sources'; # Path to your Source directory without trailing /!
-$theme_dir = 'C:\\wamp\\www\\nSnow\\Themes'; # Path to your Themes directory without trailing /!
-$language_dir = 'C:\\wamp\\www\\nSnow\\Languages'; # Path to your Languages directory without trailing /!
-$cmsurl = 'http://localhost/nSnow/'; # URL to your SnowCMS Installation
-$theme_url = 'http://localhost/nSnow/Themes/'; # URL to your SnowCMS Themes folder
+$source_dir = ''; # Path to your Source directory without trailing /!
+$theme_dir = ''; # Path to your Themes directory without trailing /!
+$language_dir = ''; # Path to your Languages directory without trailing /!
+$cmsurl = ''; # URL to your SnowCMS Installation
+$theme_url = ''; # URL to your SnowCMS Themes folder
 
 /* Don't touch the stuff below! */
 $db_prefix = '`'.$mysql_db.'`.'.$mysql_prefix;
