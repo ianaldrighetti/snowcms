bff204c9090d31b1b3bf2d88b5d628028ef817b7
nscnetworks <nscnetworks@f10a5a2d-b74f-0410-8a28-69e04d5e54c3>
2008-06-25 08:12:40 +0000

    updated

--- trunk/Sources/BoardIndex.php
+++ trunk/Sources/BoardIndex.php
@@ -9,56 +9,57 @@ function BoardIndex() {
 global $cmsurl, $db_prefix, $l, $settings, $user;
   if(can('view_forum')) {
     // Get All the categories! :D!
-    $result = mysql_query("SELECT * FROM {$db_prefix}categories ORDER BY `corder` ASC");
+    $result = mysql_query("SELECT * FROM {$db_prefix}categories ORDER BY `corder` ASC") or die(mysql_error());
     $cats = array();
       while($row = mysql_fetch_assoc($result)) {
         $cats[$row['cid']] = array(
           'id' => $row['cid'],
           'name' => $row['cname'],
-          'desc' => $row['cdesc']
+          'desc' => $row['cdesc'],
+          'boards' => array()
         );
       }
-    // Get all the boards :)
-    $result = mysql_query("SELECT * FROM {$db_prefix}boards ORDER BY `border` ASC");
-    while($row = mysql_fetch_assoc($result)) {
-      $cats[$row['cid']][$row['bid']] = array(
-        'id' => $row['bid'],
-        'catid' => $row['cid'],
-        'who_view' => @trim(@explode(",", $row['who_view'])),
-        'name' => $row['name'],
-        'desc' => $row['bdesc'],
-        'numtopics' => $row['numtopics'],
-        'numposts' => $row['numposts']
-      );
-    }
+    $result = mysql_query("
+      SELECT 
+        b.bid, b.name, b.bdesc, b.who_view, b.numtopics, b.numposts,
+        b.cid, log.uid, log.new AS is_new
+      FROM {$db_prefix}boards AS b
+        LEFT JOIN {$db_prefix}board_logs AS log ON b.bid = {$user['id']}
+      ORDER BY b.border ASC") or die(mysql_error());
+      while($row = mysql_fetch_assoc($result)) {
+        if($row['is_new']==null)
+          $row['is_new'] = 1;
+        $cats[$row['cid']]['boards'][$row['bid']] = array(
+          'id' => $row['bid'],
+          'name' => $row['name'],
+          'desc' => $row['bdesc'],
+          'who_view' => @explode(",", $row['who_view']),
+          'topics' => $row['numtopics'],
+          'posts' => $row['numposts'],
+          'is_new' => $row['is_new'] ? true : false
+        );
+      }
     foreach($cats as $cat) {
-      if(count($cats[$cat['id']])>0) {
-        // K, their are some here, just because they are here, don't mean they are allowed to see them :P
-        foreach($cat as $board) {
-          if(is_array($board['who_view'])) {
-            if(!in_array($user['group'], $board['who_view'])) {
-              // Their user group isn't in the array =S so they can't view it, unset it!
-              unset($cats[$cat['id']][$board['id']]);
-            }
-          }
-          else {
-            // Okay, so their weren't any more then 1 user group allowed, maybe they are in it :)
-            if($user['group']!=$board['who_view']) {
-              // Or not ._.
-              unset($cats[$cat['id']][$board['id']]);
-            }
+      if(count($cats[$cat['id']]['boards'])>0) {
+        foreach($cat['boards'] as $board) {
+          // Can they view this board? :o But of course, the admin can view anything :D!
+          if((!in_array($user['group'], $board['who_view'])) && ($user['group']!=1)){
+            // They can't! UNSET it so the can't see hehe :)
+            unset($cats[$cat['id']]['boards'][$board['id']]);
           }
         }
-        // Maybe now their are no boards in that category? :0
-        if(count($cats[$cat['id']])==0) {
+        // Check if this cat has any boards again...
+        if(count($cats[$cat['id']]['boards'])==0)
           unset($cats[$cat['id']]);
-        }
       }
       else {
-        // Nothing hear... Unset it...
+        // Dont show a category if it has no boards...
         unset($cats[$cat['id']]);
       }
     }
+    $settings['forum']['cats'] = $cats;
+    $settings['page']['title'] = $l['forum_title'];
+    loadForum('BoardIndex');
   }
   else {
     $settings['page']['title'] = $l['forum_error_title'];
