cf44d1597a6b6877b413473170da9dc957e33277
nscnetworks <nscnetworks@f10a5a2d-b74f-0410-8a28-69e04d5e54c3>
2008-06-25 09:09:55 +0000

    Initially added and coded

---
+++ trunk/Sources/Board.php
@@ -0,0 +1,68 @@
+<?php
+//                 SnowCMS
+//           By aldo and soren121
+//  Founded by soren121 & co-founded by aldo
+//    http://snowcms.northsalemcrew.net
+//
+// SnowCMS is released under the GPL v3 License
+// Which means you are free to edit it and then
+//       redistribute it as your wish!
+// 
+//              Board.php file 
+
+
+if(!defined("Snow"))
+  die("Hacking Attempt...");
+  
+function loadBoard() {
+global $cmsurl, $db_prefix, $l, $settings, $user;
+  $Board_ID = addslashes(mysql_real_escape_string($_REQUEST['board']));
+  $result = mysql_query("SELECT * FROM {$db_prefix}boards WHERE `bid` = '$Board_ID'");
+    while($row = mysql_fetch_assoc($result)) {
+      $board = array(
+        'id' => $row['bid'],
+        'cid' => $row['cid'],
+        'name' => $row['name'],
+        'who_view' => @explode(",", $row['who_view'])
+      );
+    }
+  if(count($board)>0) {  
+    // Before we do anything else, are they allowed to see this board? :o!
+    if((!in_array($user['group'], $board['who_view'])) && ($user['group']!=1)) {
+      $settings['page']['title'] = $l['forum_error_title'];
+      loadForum('Error','CantViewB');    
+    }
+    else {
+      $start = 0;
+      $result = mysql_query("
+        SELECT 
+          t.tid, t.sticky, t.locked, t.bid, t.first_msg, t.last_msg, t.topic_starter, t.topic_ender, t.num_replies,
+          t.numviews, t.starter_id, t.ender_id, msg.mid, msg.tid, msg.uid, msg.subject, msg.post_time, msg.poster_name,
+          msg2.mid, msg2.tid, msg2.uid, msg2.subject, msg2.post_time, msg2.poster_name
+        FROM {$db_prefix}topics AS t
+          LEFT JOIN {$db_prefix}messages AS msg ON msg.mid = t.first_msg
+          LEFT JOIN {$db_prefix}messages AS msg2 ON msg2.mid = t.last_msg
+          LEFT JOIN {$db_prefix}members AS mem ON mem.id = t.starter_id
+          LEFT JOIN {$db_prefix}members AS mem2 ON mem2.id = t.ender_id
+        WHERE 
+          t.bid = $Board_ID
+        ORDER BY t.sticky DESC, msg2.post_time ASC") or die(mysql_error());
+        $topics = array();
+        while($row = mysql_fetch_assoc($result)) {
+          $topics[] = array(
+            'id' => $row['tid'],
+            'is_sticky' => $row['sticky'],
+            'is_locked' => $row['locked'],
+            
+          );
+        }
+      $settings['page']['title'] = $settings['site_name'].' - '.$board['name'];
+      loadForum('MessageIndex');
+    }
+  }
+  else {
+    $settings['page']['title'] = $l['forum_error_title'];
+    loadForum('Error','NoBoard');
+  }
+}
+?>
