0a65db3526795c99952ea949e357eeb93a0e4f1e
nscnetworks <nscnetworks@f10a5a2d-b74f-0410-8a28-69e04d5e54c3>
2008-06-25 00:13:31 +0000

    Removed listpages actions

--- trunk/index.php
+++ trunk/index.php
@@ -59,7 +59,6 @@ require_once($source_dir.'/Core.php');
       'profile' => array('Profile.php','Profile'),
       'register' => array('Register.php','Register'),
       'register2' => array('Register.php','Register2'),
-	  'listpage' => array('Page.php','PageList')
     );
     // Is this action they are requesting even in the $actions array? If not, we don't want an error message.
     // So, lets load the Home() Function
