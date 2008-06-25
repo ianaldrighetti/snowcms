477887edf611842e699a3210253e73be4ef7996b
nscnetworks <nscnetworks@f10a5a2d-b74f-0410-8a28-69e04d5e54c3>
2008-06-25 06:16:17 +0000

    added $settings['page']['title'] between <title> tags

--- trunk/Themes/default/Forum.template.php
+++ trunk/Themes/default/Forum.template.php
@@ -11,7 +11,7 @@ echo '
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<title>Topic Display</title>
+	<title>'.$settings['page']['title'].'</title>
   <link rel="stylesheet" href="'.$theme_url.'/'.$settings['theme'].'/forum.css" type="text/css" media="screen" />
 </head>
 <body>
