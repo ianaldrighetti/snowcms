bc94d203d791a30183d8bffa22c4f4398752b54e
nscnetworks <nscnetworks@f10a5a2d-b74f-0410-8a28-69e04d5e54c3>
2008-06-25 20:07:15 +0000

    updated

--- trunk/Sources/Board.php
+++ trunk/Sources/Board.php
@@ -36,9 +36,11 @@ global $cmsurl, $db_prefix, $l, $settings, $user;
       $start = 0;
       $result = mysql_query("
         SELECT 
-          t.tid, t.sticky, t.locked, t.bid, t.first_msg, t.last_msg, t.topic_starter, t.topic_ender, t.num_replies,
-          t.numviews, t.starter_id, t.ender_id, msg.mid, msg.tid, msg.uid, msg.subject, msg.post_time, msg.poster_name,
-          msg2.mid, msg2.tid, msg2.uid, msg2.subject, msg2.post_time, msg2.poster_name
+          t.tid, t.sticky, t.locked, t.bid, t.first_msg, t.last_msg, IFNULL(t.last_msg, t.first_msg) AS last_msg, t.topic_starter, t.topic_ender AS topic_ender, IFNULL(t.topic_ender, t.topic_starter) AS topic_ender, t.num_replies,
+          t.numviews, t.starter_id, t.ender_id, IFNULL(t.ender_id, t.starter_id) AS ender_id, msg.mid, msg.tid, msg.uid, msg.subject, msg.post_time, msg.poster_name,
+          msg2.mid AS mid2, IFNULL(msg2.mid, msg.mid) AS mid2, msg2.uid AS uid2, IFNULL(msg2.uid, msg.uid) AS uid2, 
+          msg2.subject AS subject2, IFNULL(msg2.subject, msg.subject) AS subject2, msg2.post_time AS last_post_time, IFNULL(msg2.post_time, msg.post_time) AS last_post_time,
+          msg2.poster_name AS poster_name2, IFNULL(msg2.poster_name, msg.poster_name) AS poster_name2
         FROM {$db_prefix}topics AS t
           LEFT JOIN {$db_prefix}messages AS msg ON msg.mid = t.first_msg
           LEFT JOIN {$db_prefix}messages AS msg2 ON msg2.mid = t.last_msg
@@ -46,16 +48,12 @@ global $cmsurl, $db_prefix, $l, $settings, $user;
           LEFT JOIN {$db_prefix}members AS mem2 ON mem2.id = t.ender_id
         WHERE 
           t.bid = $Board_ID
-        ORDER BY t.sticky DESC, msg2.post_time ASC") or die(mysql_error());
+        ORDER BY t.sticky DESC, last_post_time ASC") or die(mysql_error());
         $topics = array();
         while($row = mysql_fetch_assoc($result)) {
-          $topics[] = array(
-            'id' => $row['tid'],
-            'is_sticky' => $row['sticky'],
-            'is_locked' => $row['locked'],
-            
-          );
+          $topics[] = $row;
         }
+      $settings['topics'] = $topics;
       $settings['page']['title'] = $settings['site_name'].' - '.$board['name'];
       loadForum('MessageIndex');
     }
