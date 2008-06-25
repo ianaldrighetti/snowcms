8acc4f2ff2d177b134f47ae556c6543f44960d85
nscnetworks <nscnetworks@f10a5a2d-b74f-0410-8a28-69e04d5e54c3>
2008-06-25 09:10:37 +0000

    Made it so the Admin (UID #1) can view every board no matter what

--- trunk/Sources/BoardIndex.php
+++ trunk/Sources/BoardIndex.php
@@ -1,7 +1,17 @@
 <?php
+//                 SnowCMS
+//           By aldo and soren121
+//  Founded by soren121 & co-founded by aldo
+//    http://snowcms.northsalemcrew.net
+//
+// SnowCMS is released under the GPL v3 License
+// Which means you are free to edit it and then
+//       redistribute it as your wish!
+// 
+//           BoardIndex.php file 
 
 
-if(!defined('Snow'))
+if(!defined("Snow"))
   die("Hacking Attempt...");
 
 // This function loads up the forum index  
