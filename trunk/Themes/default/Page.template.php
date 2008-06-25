50fb13d3aee68f0b06a9d7a807e38952648ef3df
nscnetworks <nscnetworks@f10a5a2d-b74f-0410-8a28-69e04d5e54c3>
2008-06-25 00:27:53 +0000

    Removed some stuff

--- trunk/Themes/default/Page.template.php
+++ trunk/Themes/default/Page.template.php
@@ -8,13 +8,8 @@ function Main() {
 global $cmsurl, $l, $settings, $user;
   echo '
 		<h2>'.$settings['page']['title'].'</h2>
-		<hr>';
-  if ($settings['page']['show_info'] == 1)
-		echo '<small> by '.$settings['page']['owner'].' | '.$settings['page']['date'].'</small>';
-  echo	'
 		<p>'.$settings['page']['content'].'</p> 
-		<br />
-		';
+		<br />';
 }
 
 function Error() {
