8b5679461d3653346db16317b356bbdeac23836b
nscnetworks <nscnetworks@f10a5a2d-b74f-0410-8a28-69e04d5e54c3>
2008-06-27 08:08:59 +0000

    Added code

--- trunk/Sources/Profile.php
+++ trunk/Sources/Profile.php
@@ -16,5 +16,55 @@ if(!defined("Snow"))
 
 function Profile() {
 global $cmsurl, $db_prefix, $l, $settings, $source_dir, $user;
+  // Maybe they are trying to view someone's profile? o.O
+  if((can('view_profile')) && ((!empty($_REQUEST['u'])) && ($_REQUEST['u']!=$user['id']))) {
+    $UID = (int)addlashes(mysql_real_escape_string($_REQUEST['u']));
+    $result = mysql_query("
+       SELECT
+         m.id, m.username, m.email, m.display_name, m.reg_date, m.reg_ip, m.last_login,
+         m.last_ip, m.group, m.numposts, m.signature, m.activated, grp.group_id, 
+         grp.groupname
+       FROM {$db_prefix}members AS m
+         LEFT JOIN {$db_prefix}membergroups AS grp ON grp.group_id = m.group
+       WHERE m.id = $UID");
+    // Hmmm, is this account in this DB? D:
+    if(mysql_num_rows($result)) {
+      // It exists! :D
+      while($row = mysql_fetch_assoc($result)) {
+        $mem = array(
+          'id' => $row['id'],
+          'name' => $row['display_name'] ? $row['display_name'] : $row['username'],
+          'email' => $row['email'],
+          'reg_date' => formattime($row['reg_date']),
+          'ip' => $row['last_ip'] ? $row['last_ip'] : $row['reg_ip'],
+          'membergroup' => $row['groupname'],
+          'group_id' => $row['group'],
+          'numposts' => $row['numposts'],
+          'signature' => $row['signature'],
+          'activated' => $row['activated']
+        );
+      }
+      $settings['page']['title'] = str_replace("%user%", $mem['name'], $l['profile_profile_of']);
+      $settings['profile'] = $mem;
+      loadTheme('Profile','View');
+    }
+    else {
+      // Oh noes! It doesnt! Tell'em :P
+      $settings['page']['title'] = $l['profile_error_title'];
+      loadTheme('Profile','NoProfile');
+    }
+  }
+  else
+  // Are they logged in? .-.
+  if($user['is_logged']) {
+    // Hmmm, Are 
+    $settings['page']['title'] = $l['profile_title'];
+    loadTheme('Profile');
+  }
+  else {
+    // No! Go away! :)
+    $settings['page']['title'] = $l['profile_error_title'];
+    loadTheme('Profile','NotAllowed');
+  }
 }
 ?>
