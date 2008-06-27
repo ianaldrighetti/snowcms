bdc69eda4d268657c50b19f107951250e4c523b4
nscnetworks <nscnetworks@f10a5a2d-b74f-0410-8a28-69e04d5e54c3>
2008-06-27 08:09:21 +0000

    Initially added

---
+++ trunk/Themes/default/Profile.template.php
@@ -0,0 +1,16 @@
+<?php
+// default/Profile.template.php by SnowCMS Dev's
+
+if(!defined('Snow'))
+  die("Hacking Attempt...");
+  
+function Main() {
+
+}
+
+function NotAllowed() {
+global $cmsurl, $l, $settings;
+echo '
+  <h1>'.$l['profile_error_header'].'</h1>
+  <p>'.$l['profile_error_desc'].'</p>';
+}
