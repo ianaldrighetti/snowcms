ecf82f31d0e5467d46f36f1dacf84ea8ebf21e18
nscnetworks <nscnetworks@f10a5a2d-b74f-0410-8a28-69e04d5e54c3>
2008-06-28 07:49:16 +0000

    Initially added, only has debug information

---
+++ trunk/Themes/default/Permissions.template.php
@@ -0,0 +1,11 @@
+<?php
+// default/Permissions.template.php by SnowCMS Dev's
+
+if(!defined('Snow'))
+  die("Hacking Attempt...");
+  
+function Main() {
+global $cmsurl, $settings, $user;
+  print_r($settings['groups']);
+}
+?>
