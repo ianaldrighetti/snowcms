9a38659672348c8bd414c46705c59648802bf5c6
nscnetworks <nscnetworks@f10a5a2d-b74f-0410-8a28-69e04d5e54c3>
2008-06-28 08:38:55 +0000

    Made it follow permissions, and changed how page_id is sanitized

--- trunk/Sources/Page.php
+++ trunk/Sources/Page.php
@@ -44,126 +44,134 @@ global $cmsurl, $db_prefix, $l, $settings, $user;
 // An Admin Function to Make/Manage Pages
 function ManagePages() {
 global $cmsurl, $db_prefix, $l, $settings, $user;
-  $settings['page']['make_page']['do'] = false;
-  $settings['page']['update_page'] = 0;
-  // Do we need to update a page?
-  if(!empty($_REQUEST['update_page'])) {
-    // If so, get the info, and clean it :P
-    $page_id = clean($_REQUEST['page_id']);
-    $page_title = clean($_REQUEST['page_title']);
-    $page_content = addslashes($_REQUEST['page_content']);
-	if(isset($_REQUEST['page_show_info']))
-		$page_show_info = $_REQUEST['page_show_info'];
-	else
-		$page_show_info = 0;
-    // Update it
-    $result = mysql_query("UPDATE {$db_prefix}pages SET `title` = '{$page_title}', `content` = '{$page_content}', `show_info` = '{$page_show_info}' WHERE `page_id` = '{$page_id}'");
-    if($result) {
-      // It was successful!
-      $settings['page']['update_page'] = 1;
+  if(can('manage_pages')) { 
+    $settings['page']['make_page']['do'] = false;
+    $settings['page']['update_page'] = 0;
+    // Do we need to update a page?
+    if(!empty($_REQUEST['update_page'])) {
+      // If so, get the info, and clean it :P
+      $page_id = clean($_REQUEST['page_id']);
+      $page_title = clean($_REQUEST['page_title']);
+      $page_content = addslashes($_REQUEST['page_content']);
+	  if(isset($_REQUEST['page_show_info']))
+	  	$page_show_info = $_REQUEST['page_show_info'];
+	  else
+		  $page_show_info = 0;
+      // Update it
+      $result = mysql_query("UPDATE {$db_prefix}pages SET `title` = '{$page_title}', `content` = '{$page_content}', `show_info` = '{$page_show_info}' WHERE `page_id` = '{$page_id}'");
+      if($result) {
+        // It was successful!
+        $settings['page']['update_page'] = 1;
+      }
+      else {
+        // Wasn't Successful! Oh Noes!
+        $settings['page']['update_page'] = 2;
+      }
     }
-    else {
-      // Wasn't Successful! Oh Noes!
-      $settings['page']['update_page'] = 2;
+    // Or are we supposed to create a page?
+    if(!empty($_REQUEST['make_page'])) {
+      $settings['page']['make_page']['do'] = true;  
+      // Who is the Page Owner? (Ther User ID)
+      $page_owner = clean($user['id']);
+      // We save their name, just incase if their account is deleted
+      $owner_name = clean($user['name']);
+      // The Time stamp of when it was made
+      $create_date = time();
+      // Clean the page's title
+      $title = clean($_REQUEST['page_title']);
+      // Insert it
+      $result = mysql_query("INSERT INTO {$db_prefix}pages (`page_owner`,`owner_name`,`create_date`,`title`) VALUES('{$page_owner}','{$owner_name}','{$create_date}','{$title}')");
+      if($result) {
+        // It was a Success! Wooot!
+        $settings['page']['make_page']['status'] = true;
+        $settings['page']['make_page']['title'] = $title;
+        $settings['page']['make_page']['info'] = str_replace("%title%", $_REQUEST['page_title'], $l['adminpage_make_success']);
+      }
+      else {
+        // Oh NOES! It failed!
+        $settings['page']['make_page']['status'] = false;
+        $settings['page']['make_page']['title'] = $title;
+        $settings['page']['make_page']['info'] = str_replace("%title%", $_REQUEST['page_title'], $l['adminpage_make_fail']);
+      }
     }
+    // Get all the pages in the database so we can list them :)
+    $result = mysql_query("
+       SELECT
+         p.page_id, p.page_owner, p.owner_name, p.create_date, 
+         p.modify_date, p.title, m.id, m.username, m.display_name
+       FROM {$db_prefix}pages AS p
+         LEFT JOIN {$db_prefix}members AS m ON m.id = p.page_owner
+       ORDER BY p.page_id DESC") or die(mysql_error());
+      $pages = array();
+      while($row = mysql_fetch_assoc($result)) {
+        if(!$row['id'])
+          $owner = $row['owner_name'];
+        elseif($row['display_name']!=null)
+          $owner = $row['display_name'];
+        else
+          $owner = $row['username'];
+        $pages[] = array(
+          'page_id' => $row['page_id'],
+          'title' => $row['title'],
+          'page_owner' => $row['page_owner'],
+          'owner' => $owner,
+          'date' => $row['modify_date'] ? formattime($row['modify_date']) : formattime($row['create_date'])
+        );          
+      }
+    // Load the $pages array into $settings so we can pass it on
+    $settings['page']['pages'] = $pages;
+    // Lets make it simple, count how many pages their are
+    $settings['page']['num_pages'] = count($pages);
+    // Set page title, and load ManagePages template
+    $settings['page']['title'] = $l['adminpage_make_title'];
+    loadTheme('ManagePages');
   }
-  // Or are we supposed to create a page?
-  if(!empty($_REQUEST['make_page'])) {
-    $settings['page']['make_page']['do'] = true;  
-    // Who is the Page Owner? (Ther User ID)
-    $page_owner = clean($user['id']);
-    // We save their name, just incase if their account is deleted
-    $owner_name = clean($user['name']);
-    // The Time stamp of when it was made
-    $create_date = time();
-    // Clean the page's title
-    $title = clean($_REQUEST['page_title']);
-    // Insert it
-    $result = mysql_query("INSERT INTO {$db_prefix}pages (`page_owner`,`owner_name`,`create_date`,`title`) VALUES('{$page_owner}','{$owner_name}','{$create_date}','{$title}')");
-    if($result) {
-      // It was a Success! Wooot!
-      $settings['page']['make_page']['status'] = true;
-      $settings['page']['make_page']['title'] = $title;
-      $settings['page']['make_page']['info'] = str_replace("%title%", $_REQUEST['page_title'], $l['adminpage_make_success']);
-    }
-    else {
-      // Oh NOES! It failed!
-      $settings['page']['make_page']['status'] = false;
-      $settings['page']['make_page']['title'] = $title;
-      $settings['page']['make_page']['info'] = str_replace("%title%", $_REQUEST['page_title'], $l['adminpage_make_fail']);
-    }
+  else {
+    $settings['page']['title'] = $l['admin_error_title'];
+    loadTheme('Admin','Error');
   }
-  // Get all the pages in the database so we can list them :)
-  $result = mysql_query("
-     SELECT
-       p.page_id, p.page_owner, p.owner_name, p.create_date, 
-       p.modify_date, p.title, m.id, m.username, m.display_name
-     FROM {$db_prefix}pages AS p
-       LEFT JOIN {$db_prefix}members AS m ON m.id = p.page_owner
-     ORDER BY p.page_id DESC") or die(mysql_error());
-    $pages = array();
-    while($row = mysql_fetch_assoc($result)) {
-      if(!$row['id'])
-        $owner = $row['owner_name'];
-      elseif($row['display_name']!=null)
-        $owner = $row['display_name'];
-      else
-        $owner = $row['username'];
-      $pages[] = array(
-        'page_id' => $row['page_id'],
-        'title' => $row['title'],
-        'page_owner' => $row['page_owner'],
-        'owner' => $owner,
-        'date' => $row['modify_date'] ? formattime($row['modify_date']) : formattime($row['create_date'])
-      );
-        
-    }
-  // Load the $pages array into $settings so we can pass it on
-  $settings['page']['pages'] = $pages;
-  // Lets make it simple, count how many pages their are
-  $settings['page']['num_pages'] = count($pages);
-  // Set page title, and load ManagePages template
-  $settings['page']['title'] = $l['adminpage_make_title'];
-  loadTheme('ManagePages');
 }
 
 function EditPage() {
 global $cmsurl, $db_prefix, $l, $settings, $user;
-  // Get the Page ID and clean it!
-  $page_id = clean($_REQUEST['page_id']);
-  if(!empty($page_id)) {
-    // Get it!
-    $result = mysql_query("SELECT * FROM {$db_prefix}pages WHERE `page_id` = '{$page_id}'");
-    // Does it exist? o.O
-    if(mysql_num_rows($result)>0) {
-      while($row = mysql_fetch_assoc($result)) {
-        $page = array(
-          'page_id' => $row['page_id'],
-          'title' => $row['title'],
-          'content' => stripslashes($row['content'])
-        );
-        // Load $page (the pages info) into $settings, clean() the content with clean() so it won't parse any HTML Entities like &copy; as what you would see (c)
-        // Set the title, and load the ManagePages template with the Editor function
-        $settings['page']['edit_page'] = $page;
-        $settings['page']['edit_page']['content'] = clean($settings['page']['edit_page']['content']);
-        $settings['page']['title'] = str_replace("%title%", $page['title'], $l['managepages_edit_title']);
-		if($settings['page']['edit_page']['show_info'] == 1)
-			$settings['page']['edit_page']['checked'] = "checked";
-		else
-			$settings['page']['edit_page']['checked'] = "";
-        loadTheme('ManagePages','Editor');
+  if(can('manage_pages')) {  
+    // Get the Page ID and clean it!
+    $page_id = (int)addslashes(mysql_real_escape_string($_REQUEST['page_id']));
+    if(!empty($page_id)) {
+      // Get it!
+      $result = mysql_query("SELECT * FROM {$db_prefix}pages WHERE `page_id` = '{$page_id}'");
+      // Does it exist? o.O
+      if(mysql_num_rows($result)>0) {
+        while($row = mysql_fetch_assoc($result)) {
+          $page = array(
+            'page_id' => $row['page_id'],
+            'title' => $row['title'],
+            'content' => stripslashes($row['content'])
+          );
+          // Load $page (the pages info) into $settings, clean() the content with clean() so it won't parse any HTML Entities like &copy; as what you would see (c)
+          // Set the title, and load the ManagePages template with the Editor function
+          $settings['page']['edit_page'] = $page;
+          $settings['page']['edit_page']['content'] = clean($settings['page']['edit_page']['content']);
+          $settings['page']['title'] = str_replace("%title%", $page['title'], $l['managepages_edit_title']);
+          loadTheme('ManagePages','Editor');
+        }
+      }
+      else {
+        // The Page doesn't exist! Load up the Error :P
+        $settings['page']['title'] = $l['managepages_no_page_title'];
+        loadTheme('ManagePages','NoPage');
       }
     }
     else {
-      // The Page doesn't exist! Load up the Error :P
+      // $page_id is empty, load the error...
       $settings['page']['title'] = $l['managepages_no_page_title'];
       loadTheme('ManagePages','NoPage');
     }
   }
   else {
-    // $page_id is empty, load the error...
-    $settings['page']['title'] = $l['managepages_no_page_title'];
-    loadTheme('ManagePages','NoPage');
+    // They cant manage pages, why should they be able to edit them? xD
+    $settings['page']['title'] = $l['admin_error_title'];
+    loadTheme('Admin','Error');
   }
 }
 ?>
