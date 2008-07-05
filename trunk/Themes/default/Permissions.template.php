9aed4d6eba0740ee50a1b76f8c5441f3bffa587a
nscnetworks <nscnetworks@f10a5a2d-b74f-0410-8a28-69e04d5e54c3>
2008-07-05 19:59:15 +0000

    Updated Edit()

--- trunk/Themes/default/Permissions.template.php
+++ trunk/Themes/default/Permissions.template.php
@@ -45,14 +45,21 @@ echo '</pre>';
   <h2>'.$l['permissions_edit_header'].'</h2>
   <p>'.$l['permissions_edit_desc'].'</p>';
   echo '
-  <table>';
+  <form action="" method="post">
+    <fieldset>
+      <table>';
   foreach($settings['permissions']['group'] as $perm) {
     echo '
-    <tr>
-      <td>'.$l['permissions_perm_'.$perm].'</td><td><input name="'.$perm.'" type="checkbox" value="1" ', @$settings['perms'][$what] ? 'checked="checked"', '/></td>
-    </tr>';
+        <tr>
+          <td>'.$l['permissions_perm_'.$perm].'</td><td><input name="'.$perm.'" type="checkbox" value="1" ', @$settings['perms'][$what] ? 'checked="checked"', '/></td>
+        </tr>';
   }
   echo '
-  </table>';
+        <tr>
+          <td>&nbsp;</td><td><input name="update_perms" type="submit" value="'.$l['permissions_edit_save'].'"/></td>
+        </tr>
+      </table>
+    </fieldset>
+  </form>';
 }
 ?>
