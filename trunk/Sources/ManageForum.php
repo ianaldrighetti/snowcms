61571033548450478bdeb745aa8c059d730946d6
nscnetworks <nscnetworks@f10a5a2d-b74f-0410-8a28-69e04d5e54c3>
2008-07-08 04:08:30 +0000

    Initially added

---
+++ trunk/Sources/ManageForum.php
@@ -0,0 +1,35 @@
+<?php
+//                 SnowCMS
+//           By aldo and soren121
+//  Founded by soren121 & co-founded by aldo
+//    http://snowcms.northsalemcrew.net
+//
+// SnowCMS is released under the GPL v3 License
+// Which means you are free to edit it and then
+//       redistribute it as your wish!
+// 
+//           ManageForum.php file 
+
+
+if(!defined("Snow"))
+  die("Hacking Attempt...");
+  
+// Displays the page
+function ManageForum() {
+global $cmsurl, $db_prefix, $l, $settings, $user;
+  // :O We need another sub action kind of thing don't we? .-.
+  // Lets call it Forum Action, or fa for short :-]
+    $fa = array(
+      'boards' => array('ManageForum.php','ManageBoards'),
+      'categories' => array('ManageForum.php','ManageCats'),
+    );
+    if(is_array(@$fa[$_REQUEST['fa']])) {
+      require_once($source_dir.'/'.$fa[$_REQUEST['fa']][0]);
+        $fa[$_REQUEST['fa']][1]();
+    }
+    else {
+      // Its not :( We don't want an error, so show the AdminHome();
+      ForumHome();
+    }    
+}
+?>
