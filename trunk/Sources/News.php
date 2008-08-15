c353eeda4b1c93c0ba08309f322f4604a1732620
nscnetworks <nscnetworks@f10a5a2d-b74f-0410-8a28-69e04d5e54c3>
2008-08-15 03:26:25 +0000

    Added support for showing news by ID

--- trunk/Sources/News.php
+++ trunk/Sources/News.php
@@ -46,13 +46,58 @@ global $cmsurl, $db_prefix, $l, $settings, $user;
         );
         mysql_free_result($result);
       }
+      // Load it up :D (the theme thingy)
+      $settings['page']['title'] = $l['news_title'];
+      $settings['news'] = $news;
+      unset($news);
+      loadTheme('News');
     }
     else {
       // No news? :O
+      $settings['page']['title'] = $l['news_title'];
+      loadTheme('News','None');
     }
   }
   else {
-  
+    // What News do they want?
+    $news_id = (int)$_REQUEST['id'];
+    $result = sql_query("
+      SELECT
+        n.news_id AS id, n.poster_id, n.poster_name, n.subject, n.body, n.body,
+        n.modify_time AS post_date, IFNULL(n.modify_time, n.post_time) AS post_date,
+        n.numViews, n.numComments, n.allow_comments,
+        mem.id, mem.display_name AS username, IFNULL(mem.display_name, mem.username) AS username
+      FROM {$db_prefix}news AS n
+        LEFT JOIN {$db_prefix}members AS mem ON mem.id = n.poster_id
+      WHERE n.news_id = $news_id");
+    $news = array();
+    // Is there even any news? :O
+    if(mysql_num_rows($result)) {
+      while($row = mysql_fetch_assoc($result)) {
+        $news = array(
+          'id' => $row['news_id'],
+          'poster_id' => $row['poster_id'],
+          'poster_name' => $row['username'],
+          'subject' => $row['subject'],
+          'body' => stripslashes($row['body']),
+          'post_date' => formattime($row['post_date']),
+          'numViews' => $row['numViews'],
+          'numComments' => $row['numComments'],
+          'allow_comments' => $row['allow_comments']
+        );
+        mysql_free_result($result);
+      }
+      // Load it up :D (the theme thingy)
+      $settings['page']['title'] = $news['subject'];
+      $settings['news'] = $news;
+      unset($news);
+      loadTheme('News','Single');
+    }
+    else {
+      // It doesn't exist? :O
+      $settings['page']['title'] = $l['news_title'];
+      loadTheme('News','DoesntExist');
+    }  
   }
 }
 ?>
