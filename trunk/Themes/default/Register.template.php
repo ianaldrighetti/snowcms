a4fb0e2ecb3c54b037f0cdfe6e42fd83edb930a1
nscnetworks <nscnetworks@f10a5a2d-b74f-0410-8a28-69e04d5e54c3>
2008-08-15 23:39:22 +0000

    Partial Bugfix

--- trunk/Themes/default/Register.template.php
+++ trunk/Themes/default/Register.template.php
@@ -11,7 +11,7 @@ global $cmsurl, $settings, $l, $user;
   <p>'.$l['register_details'].'</p>
   <script type="text/javascript" src="includes/jquery.js"></script>
   <script type="text/javascript" src="includes/jquery-pstrength.js"></script>
-  <script type="text/javascript">$(function() {$('.password').pstrength();});</script>
+  <script type="text/javascript">$(function() {$('.password.').pstrength();});</script>
   <form action="'.$cmsurl.'index.php?action=register2" method="post">
     <fieldset>
       <table>';
