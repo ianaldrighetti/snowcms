ad922004ca88da020708580b54b08d6329244dd7
nscnetworks <nscnetworks@f10a5a2d-b74f-0410-8a28-69e04d5e54c3>
2008-08-16 00:06:01 +0000

    removed strtolower

--- trunk/Languages/english.language.php
+++ trunk/Languages/english.language.php
@@ -26,7 +26,7 @@ $l['register_password'] = 'Password:';
 $l['register_verify_password'] = 'Verify Password:';
 $l['register_email'] = 'Email:';
 $l['register_captcha'] = 'Enter the Text you see in the image';
-$l['register_success'] = 'Thank you '.strtolower(@$_REQUEST['username']).'! Your account has been created successfully and you may now <a href="'.$cmsurl.'index.php?action=login">login</a>';
+$l['register_success'] = 'Thank you '. @$_REQUEST['username'].'! Your account has been created successfully and you may now <a href="'.$cmsurl.'index.php?action=login">login</a>';
 $l['register_successbut1'] = 'Thanks for registering! The administrators require you to activate your account via email, please check your email you used on your account, and click the link to activate your account';
 $l['register_successbut2'] = 'Thanks for registering! The administrators require themselves to activate accounts; you will recieve an email once your account is activated.';
 $l['register_failed'] = 'Registration failed!';
