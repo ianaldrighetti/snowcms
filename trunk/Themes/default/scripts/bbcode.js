a4581b83c4998a0cb86f1bd7b3423860f4b26291
nscnetworks <nscnetworks@f10a5a2d-b74f-0410-8a28-69e04d5e54c3>
2008-08-22 23:02:02 +0000

    Added new folder and moved files

---
+++ trunk/Themes/default/scripts/bbcode.js
@@ -0,0 +1,26 @@
+function add_bbcode(div_id, before, after) {
+ textarea = document.getElementById(div_id);
+ if (textarea.selection != "undefined" && textarea.createTextRange) {
+  was_empty = textarea.selection.text.length;
+  textarea.selection.text = textarea.selection.text.charAt(textarea.selection.text.length - 1) == ' ' ? before + textarea.selection.text + after + ' ' : before + textarea.selection.text + after;
+  if (was_empty) {
+   textarea.selection.moveStart("character", -after.length);
+   textarea.selection.moveEnd("character", -after.length);
+   textarea.selection.select();
+  }
+  else
+   textarea.focus(textarea.selection);
+ }
+ else if (textarea.selectionStart != "undefined") {
+  str = textarea.value.substring(0,textarea.selectionStart);
+  str += before;
+  str += textarea.value.substring(textarea.selectionStart,textarea.selectionEnd);
+  str += after;
+  str += textarea.value.substring(textarea.selectionEnd);
+  textarea.value = str;
+ }
+ else {
+  textarea.value += before + after;
+  textarea.focus(textarea.value.length - 1);
+ }
+}
