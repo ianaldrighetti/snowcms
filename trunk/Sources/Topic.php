3d0648c95b55fe69ee2976a7596883f215e3b6cc
nscnetworks <nscnetworks@f10a5a2d-b74f-0410-8a28-69e04d5e54c3>
2008-08-24 01:05:10 +0000

    Permissions fix

--- trunk/Sources/Topic.php
+++ trunk/Sources/Topic.php
@@ -16,126 +16,132 @@ if(!defined("Snow"))
   
 function loadTopic() {
 global $cmsurl, $db_prefix, $l, $settings, $user;
-  $Topic_ID = (int)$_REQUEST['topic'];
-  $result = sql_query("
-    SELECT 
-      t.tid, t.bid, b.who_view, b.bid
-    FROM {$db_prefix}topics AS t
-      LEFT JOIN {$db_prefix}boards AS b ON b.bid = t.bid
-    WHERE t.tid = '$Topic_ID' AND {$user['board_query']}");
-  // Can they even view this topic? As in, is this in a board they aren't allowed to view?
-  if(mysql_num_rows($result)) {
-    if(!empty($_REQUEST['msg'])) {
-      // The fun part! WEEEEEEEEEE!
-      // We need to redirect ?topic=topic_id;msg=msg_id to the right paginated page...
-      $msg_id = (int)$_REQUEST['msg'];
-      $topic_id = $Topic_ID;
-      // Select all messages with the tid of $topic_id
-      $result = sql_query("
-        SELECT
-          msg.mid, msg.tid
-        FROM {$db_prefix}messages AS msg
-        WHERE msg.tid = $topic_id");
-      // How many messages are there? :o
-      $num_msg = mysql_num_rows($result)-1;
-      // If the number of messages is less then $settings['topic_posts_per_page'], no need to do anything fancy :]
-      if($num_msg<$settings['topic_posts_per_page']) {
-        redirect("forum.php?topic={$topic_id}#mid{$msg_id}");
-      }
-      else {
-        $num_pages = ceil($num_msg/$settings['topic_posts_per_page']);
-        $mids = array();
-        while($row = mysql_fetch_assoc($result))
-          $mids[] = $row['mid'];
-        // If $mid_page is not 0, then we found the page =D!
-        if(in_array($msg_id, $mids)) {
-          $msgs = 0;
-          $page = 1;
-          $mid_page = 0;
-          foreach($mids as $i => $mid_val) {
-            $msgs++;
-            if($mids[$i]==$msg_id) {
-             $mid_page = $page;
-            }
-            if($msgs==$settings['topic_posts_per_page']) {
-              $msgs = 1;
-              $page++;
-            }
-          }
-          // !!! $page_id != 1 BAD!
-          if($mid_page!=1)
-            redirect("forum.php?topic={$topic_id};page={$mid_page}#mid{$msg_id}");
-          else
-            redirect("forum.php?topic={$topic_id}#mid{$msg_id}");
+  if(can('view_forum')) {
+    $Topic_ID = (int)$_REQUEST['topic'];
+    $result = sql_query("
+      SELECT 
+        t.tid, t.bid, b.who_view, b.bid
+      FROM {$db_prefix}topics AS t
+        LEFT JOIN {$db_prefix}boards AS b ON b.bid = t.bid
+      WHERE t.tid = '$Topic_ID' AND {$user['board_query']}");
+    // Can they even view this topic? As in, is this in a board they aren't allowed to view?
+    if(mysql_num_rows($result)) {
+      if(!empty($_REQUEST['msg'])) {
+        // The fun part! WEEEEEEEEEE!
+        // We need to redirect ?topic=topic_id;msg=msg_id to the right paginated page...
+        $msg_id = (int)$_REQUEST['msg'];
+        $topic_id = $Topic_ID;
+        // Select all messages with the tid of $topic_id
+        $result = sql_query("
+          SELECT
+            msg.mid, msg.tid
+          FROM {$db_prefix}messages AS msg
+          WHERE msg.tid = $topic_id");
+        // How many messages are there? :o
+        $num_msg = mysql_num_rows($result)-1;
+        // If the number of messages is less then $settings['topic_posts_per_page'], no need to do anything fancy :]
+        if($num_msg<$settings['topic_posts_per_page']) {
+          redirect("forum.php?topic={$topic_id}#mid{$msg_id}");
         }
         else {
-          // Sorry bub, we didn't find that Message ID...
-          redirect("forum.php?topic={$topic_id}");
+          $num_pages = ceil($num_msg/$settings['topic_posts_per_page']);
+          $mids = array();
+          while($row = mysql_fetch_assoc($result))
+            $mids[] = $row['mid'];
+          // If $mid_page is not 0, then we found the page =D!
+          if(in_array($msg_id, $mids)) {
+            $msgs = 0;
+            $page = 1;
+            $mid_page = 0;
+            foreach($mids as $i => $mid_val) {
+              $msgs++;
+              if($mids[$i]==$msg_id) {
+               $mid_page = $page;
+              }
+              if($msgs==$settings['topic_posts_per_page']) {
+                $msgs = 1;
+                $page++;
+              }
+            }
+            // !!! $page_id != 1 BAD!
+            if($mid_page!=1)
+              redirect("forum.php?topic={$topic_id};page={$mid_page}#mid{$msg_id}");
+            else
+              redirect("forum.php?topic={$topic_id}#mid{$msg_id}");
+          }
+          else {
+            // Sorry bub, we didn't find that Message ID...
+            redirect("forum.php?topic={$topic_id}");
+          }
         }
       }
+      // We don't want ;page=1 :P
+      if(!empty($_REQUEST['page']) && $_REQUEST['page']==1)
+        redirect("forum.php?topic={$_REQUEST['topic']}");
+      $info = paginate($Topic_ID);
+      $pagination = $info['pagination'];
+      $start = $info['start'];
+      $result = sql_query("
+        SELECT
+          t.tid, t.first_msg, msg.subject, msg.mid
+        FROM {$db_prefix}topics AS t
+          LEFT JOIN {$db_prefix}messages AS msg ON msg.mid = t.first_msg
+        WHERE t.tid = $Topic_ID");
+      while($row = mysql_fetch_assoc($result))
+        $title = $row['subject'];
+      $result = sql_query("
+        SELECT
+          t.tid, t.sticky, t.locked, t.bid, t.first_msg, grp.group_id, grp.groupname,
+          msg.mid, msg.tid, msg.bid, msg.uid, msg.subject, msg.post_time, msg.poster_name, msg.ip, msg.body,
+          mem.id AS uid, mem.username, IFNULL(mem.username, msg.poster_name) AS username, mem.display_name, 
+          mem.signature, mem.group, mem.email, mem.numposts, ol.user_id, ol.last_active
+        FROM {$db_prefix}topics AS t
+          LEFT JOIN {$db_prefix}messages AS msg ON msg.tid = t.tid
+          LEFT JOIN {$db_prefix}members AS mem ON mem.id = msg.uid
+          LEFT JOIN {$db_prefix}membergroups AS grp ON grp.group_id = mem.group
+          LEFT JOIN {$db_prefix}online AS ol ON ol.user_id = mem.id
+        WHERE t.tid = $Topic_ID
+        ORDER BY msg.mid ASC LIMIT $start,{$settings['topic_posts_per_page']}");
+        while($row = mysql_fetch_assoc($result)) {
+          if($row['display_name']!=null)
+            $row['username'] = $row['display_name'];
+          $posts[] = array(
+            'tid' => $row['tid'],
+            'mid' => $row['mid'],
+            'bid' => $row['bid'],
+            'uid' => $row['uid'],
+            'subject' => $row['subject'],
+            'post_time' => formattime($row['post_time']),
+            'body' => bbc($row['body']),
+            'username' => $row['username'] ? $row['username'] : $row['poster_name'],
+            'signature' => bbc($row['signature']),
+            'membergroup' => $row['groupname'],
+            'numposts' => $row['numposts'],
+            'status' => $row['last_active'] ? true : false,
+            'can' => array(
+                         'edit' => canforum('edit_any', $row['bid']) ? true : edit($row['uid'], canforum('edit_own', $row['bid'])),
+                         'del' => canforum('delete_any', $row['bid']) ? true : del($row['uid'], canforum('delete_own')),
+                         'split' => Splitable($row['first_msg'], $row['mid'], canforum('split_topic', $row['bid']))
+                       )
+          );
+          $bid = $row['bid'];
+        }
+        $settings['page']['title'] = $title;
+        $settings['posts'] = $posts;
+        $settings['pagination'] = $pagination;
+        $settings['bid'] = (int)$bid;
+        loadForum('Topic');
     }
-    // We don't want ;page=1 :P
-    if(!empty($_REQUEST['page']) && $_REQUEST['page']==1)
-      redirect("forum.php?topic={$_REQUEST['topic']}");
-    $info = paginate($Topic_ID);
-    $pagination = $info['pagination'];
-    $start = $info['start'];
-    $result = sql_query("
-      SELECT
-        t.tid, t.first_msg, msg.subject, msg.mid
-      FROM {$db_prefix}topics AS t
-        LEFT JOIN {$db_prefix}messages AS msg ON msg.mid = t.first_msg
-      WHERE t.tid = $Topic_ID");
-    while($row = mysql_fetch_assoc($result))
-      $title = $row['subject'];
-    $result = sql_query("
-      SELECT
-        t.tid, t.sticky, t.locked, t.bid, t.first_msg, grp.group_id, grp.groupname,
-        msg.mid, msg.tid, msg.bid, msg.uid, msg.subject, msg.post_time, msg.poster_name, msg.ip, msg.body,
-        mem.id AS uid, mem.username, IFNULL(mem.username, msg.poster_name) AS username, mem.display_name, 
-        mem.signature, mem.group, mem.email, mem.numposts, ol.user_id, ol.last_active
-      FROM {$db_prefix}topics AS t
-        LEFT JOIN {$db_prefix}messages AS msg ON msg.tid = t.tid
-        LEFT JOIN {$db_prefix}members AS mem ON mem.id = msg.uid
-        LEFT JOIN {$db_prefix}membergroups AS grp ON grp.group_id = mem.group
-        LEFT JOIN {$db_prefix}online AS ol ON ol.user_id = mem.id
-      WHERE t.tid = $Topic_ID
-      ORDER BY msg.mid ASC LIMIT $start,{$settings['topic_posts_per_page']}");
-      while($row = mysql_fetch_assoc($result)) {
-        if($row['display_name']!=null)
-          $row['username'] = $row['display_name'];
-        $posts[] = array(
-          'tid' => $row['tid'],
-          'mid' => $row['mid'],
-          'bid' => $row['bid'],
-          'uid' => $row['uid'],
-          'subject' => $row['subject'],
-          'post_time' => formattime($row['post_time']),
-          'body' => bbc($row['body']),
-          'username' => $row['username'] ? $row['username'] : $row['poster_name'],
-          'signature' => bbc($row['signature']),
-          'membergroup' => $row['groupname'],
-          'numposts' => $row['numposts'],
-          'status' => $row['last_active'] ? true : false,
-          'can' => array(
-                       'edit' => canforum('edit_any', $row['bid']) ? true : edit($row['uid'], canforum('edit_own', $row['bid'])),
-                       'del' => canforum('delete_any', $row['bid']) ? true : del($row['uid'], canforum('delete_own')),
-                       'split' => Splitable($row['first_msg'], $row['mid'], canforum('split_topic', $row['bid']))
-                     )
-        );
-        $bid = $row['bid'];
-      }
-      $settings['page']['title'] = $title;
-      $settings['posts'] = $posts;
-      $settings['pagination'] = $pagination;
-      $settings['bid'] = (int)$bid;
-      loadForum('Topic');
+    else {
+      // Sneaky one aren't you? Trying to access a topic in a board you can't access :P Well we won't have it! ^_^
+      $settings['page']['title'] = $l['forum_error_title'];    
+      loadForum('Error','TNotAllowed');
+    }
   }
   else {
-    // Sneaky one aren't you? Trying to access a topic in a board you can't access :P Well we won't have it! ^_^
-    $settings['page']['title'] = $l['forum_error_title'];    
-    loadForum('Error','TNotAllowed');
-  }     
+    $settings['page']['title'] = $l['forum_error_title'];
+    loadForum('Error','BNotAllowed');
+  }   
 }
 function paginate($topic) {
 global $cmsurl, $db_prefix, $settings;
