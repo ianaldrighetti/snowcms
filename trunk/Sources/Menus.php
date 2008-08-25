47ac5f29fd6cf82b4650824c4fa644726e6b0492
mylesgrey@programmersdelight.com <mylesgrey@programmersdelight.com@f10a5a2d-b74f-0410-8a28-69e04d5e54c3>
2008-08-25 01:14:49 +0000

    Special ordering removed until I can get all the glitches out of it

--- trunk/Sources/Menus.php
+++ trunk/Sources/Menus.php
@@ -10,53 +10,6 @@ global $l, $settings, $db_prefix;
   
   // Change links' names details
   if (@$_REQUEST['change_menus']) {
-    
-    // Perform a bunch of steps to make the order numbers whole and not skip any
-    // e.g. turn 1, 3, 3.5 and 4 into 1, 2, 3 and 4
-    $order[] = 0;
-    $total = 0;
-    $max = -1;
-    $min = -1;
-    foreach ($_REQUEST as $key => $value) {
-      if (substr($key,0,11) == 'link_order_') {
-        $link_id = clean(substr($key,11,strlen($key)));
-        $link_order = clean(@$_REQUEST['link_order_'.$link_id]);
-        $link[$link_id] = $link_order;
-        
-        if ($max < $link_id)
-          $max = $link_id + 1;
-        if ($min > $link_id || $min == -1)
-          $min = $link_id;
-      }
-    }
-    
-    if ($max == -1)
-      $max = 0;
-    
-    if (@$_REQUEST['new_link_name'] || @$_REQUEST['new_link_url']) {
-      $new_id = $max;
-      $link[$new_id] = clean(@$_REQUEST['new_link_order']);
-      $max += 1;
-    }
-    
-    $b = 1;
-    while ($b < $max - $min + 2) {
-      $smallest_value = -1;
-      $smallest_id = -1;
-      $i = 0;
-      while ($i <= $max) {
-        if (($smallest_value > @$link[$i] || $smallest_value == -1) && @$link[$i]) {
-          $smallest_value = $link[$i];
-          $smallest_id = $i;
-        }
-        $i += 1;
-      }
-      
-      $order[$smallest_id] = $b;
-      $link[$smallest_id] = 0;
-      $b += 1;
-    }
-    
     // Save changes to current links
     foreach ($_REQUEST as $key => $value)
       if (substr($key,0,10) == 'link_name_') {
@@ -67,7 +20,7 @@ global $l, $settings, $db_prefix;
         $link_main = @$_REQUEST['link_main_'.$link_id] == true;
         $link_sidebar = (@$_REQUEST['link_sidebar_'.$link_id] == true) * 2;
         $link_menu = $link_main + $link_sidebar;
-        $link_order = $order[$link_id];
+        $link_order = clean(@$_REQUEST['link_order_'.$link_id]);
         sql_query("
         UPDATE {$db_prefix}menus
         SET
@@ -88,7 +41,7 @@ global $l, $settings, $db_prefix;
       $link_main = @$_REQUEST['new_link_main'] == true;
       $link_sidebar = (@$_REQUEST['new_link_sidebar'] == true) * 2;
       $link_menu = $link_main + $link_sidebar;
-      $link_order = $order[$new_id];
+      $link_order = clean(@$_REQUEST['new_link_order']);
       sql_query("
       INSERT INTO {$db_prefix}menus
       (
@@ -107,66 +60,13 @@ global $l, $settings, $db_prefix;
       )
       ");
     }
+    redirect('index.php?action=admin;sa=menus');
   }
   // Delete a link
   if (@$_REQUEST['did']) {
     $did = clean($_REQUEST['did']);
     sql_query("DELETE FROM {$db_prefix}menus WHERE `link_id` = '$did'");
-    
-    // Perform a bunch of steps to make the order numbers whole and not skip any
-    // e.g. turn 1, 3, 3.5 and 4 into 1, 2, 3 and 4
-    $result = sql_query("SELECT * FROM {$db_prefix}menus");
-    if (mysql_num_rows($result)) {
-    while ($row = mysql_fetch_assoc($result))
-      $_REQUEST['link_order_'.$row['link_id']] = $row['order'];
-    $order[] = 0;
-    $total = 0;
-    $max = -1;
-    $min = -1;
-    foreach ($_REQUEST as $key => $value) {
-      if (substr($key,0,11) == 'link_order_') {
-        $link_id = clean(substr($key,11,strlen($key)));
-        $link_order = clean(@$_REQUEST['link_order_'.$link_id]);
-        $link[$link_id] = $link_order;
-        
-        if ($max < $link_id)
-          $max = $link_id + 1;
-        if ($min > $link_id || $min == -1)
-          $min = $link_id;
-      }
-    }
-    
-    $b = 1;
-    while ($b < $max - $min + 2) {
-      $smallest_value = -1;
-      $smallest_id = -1;
-      $i = 0;
-      while ($i <= $max) {
-        if (($smallest_value > @$link[$i] || $smallest_value == -1) && @$link[$i]) {
-          $smallest_value = $link[$i];
-          $smallest_id = $i;
-        }
-        $i += 1;
-      }
-      
-      $order[$smallest_id] = $b;
-      $link[$smallest_id] = 0;
-      $b += 1;
-    }
-    
-    foreach ($_REQUEST as $key => $value)
-      if (substr($key,0,11) == 'link_order_') {
-        $link_id = clean(substr($key,11,strlen($key)));
-        $link_order = $order[$link_id];
-        sql_query("
-        UPDATE {$db_prefix}menus
-        SET
-          `order` = '$link_order'
-        WHERE
-          `link_id` = '$link_id'
-        ");
-      }
-    }
+    redirect('index.php?action=admin;sa=menus');
   }
   
   $settings['page']['title'] = $l['managemenus_title'];
