ee303223ae883b7f74cc44622d23b8df609281a9
nscnetworks <nscnetworks@f10a5a2d-b74f-0410-8a28-69e04d5e54c3>
2008-08-27 00:15:15 +0000

    Improvements

--- trunk/image.php
+++ trunk/image.php
@@ -1,17 +1,17 @@
 <?php
 session_start();
-  error_reporting(E_ALL);
-  define("Snow", true);
-  require('config.php');
-
-  /* This is the CAPTCHA Image for registering
-     This file may disappear one day D: but of
-     course CAPTCHA will still be in SnowCMS
-     but in a different way :P
-   */
-
+error_reporting(E_ALL);
+define("Snow", true);
+require_once('config.php');
 // include captcha class
-require($source_dir.'/Captcha.php');
+require_once($source_dir.'/Captcha.php');
+/* 
+  This is the CAPTCHA Image for registering
+  This file may disappear one day D: but of
+  course CAPTCHA will still be in SnowCMS
+  but in a different way :P
+*/
+
 // define fonts
 $aFonts = array($source_dir.'/fonts/VeraBd.ttf', $source_dir.'/fonts/VeraIt.ttf', $source_dir.'/fonts/Vera.ttf');
 $captcha = new PhpCaptcha($aFonts, 200, 60);
