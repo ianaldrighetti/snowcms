1eade4df4a6678019dae630215cf727eef315dd2
antimatter15 <antimatter15@f10a5a2d-b74f-0410-8a28-69e04d5e54c3>
2008-08-27 01:39:16 +0000

    Got everything else to work at the expense of IE, will get IE to work seperately in bbcode.js soon

--- trunk/Themes/default/Post.template.php
+++ trunk/Themes/default/Post.template.php
@@ -38,7 +38,7 @@ echo '
 	  </td>
   </tr>
   <tr align="center">
-    <td colspan="2"><textarea id="body" name="body" rows="12" cols="60" onclick="this.selection = document.selection.createRange()" onkeyup="this.selection = document.selection.createRange()" onchange="this.selection = document.selection.createRange().duplicate()" onfocus="this.selection = document.selection.createRange().duplicate()">', $settings['body'], '</textarea></td>
+    <td colspan="2"><textarea id="body" name="body" rows="12" cols="60">', $settings['body'], '</textarea></td>
   </tr>';
   if(canforum('post_sticky', $settings['board']) || canforum('lock_topic', $settings['board'])) {
     if($settings['sticky'])
@@ -99,7 +99,7 @@ echo '
 	  </td>
   </tr>
   <tr align="center">
-    <td colspan="2"><textarea name="body" id="body" rows="12" cols="60" onclick="this.selection = document.selection.createRange()" onkeyup="this.selection = document.selection.createRange()" onchange="this.selection = document.selection.createRange().duplicate()" onfocus="this.selection = document.selection.createRange().duplicate()">', $settings['body'], '</textarea></td>
+    <td colspan="2"><textarea name="body" id="body" rows="12" cols="60">', $settings['body'], '</textarea></td>
   </tr>';
   if(canforum('post_sticky', $settings['board']) || canforum('lock_topic', $settings['board'])) {
     if($settings['sticky'])
