3395096e279761ebc3aa7e4a2094ea6fabd8a43c
mylesgrey@programmersdelight.com <mylesgrey@programmersdelight.com@f10a5a2d-b74f-0410-8a28-69e04d5e54c3>
2008-08-30 10:50:24 +0000

    Fixed some graphical glitches.

--- trunk/Themes/default/Post.template.php
+++ trunk/Themes/default/Post.template.php
@@ -10,13 +10,15 @@ echo '
 <form action="', $cmsurl, 'forum.php?action=post2;board=', $settings['board'], '" method="post"  class="write">
 <table id="post" border="0px">
   <tr cellspacing="0px" cellpadding="0px">
-    <td class="title" colspan="2">', $l['post_newtopic'], '</td>
+    <td class="title" colspan="3">', $l['post_newtopic'], '</td>
   </tr>
   <tr align="center">
-    <td>', $l['topic_subject'], '</td><td><input name="subject" type="text" size="80" value="', $settings['subject'], '"/>
+    <td width="20%">', $l['topic_subject'], '</td>
+    <td><input name="subject" type="text" size="78" value="', $settings['subject'], '" />
+    <td width="20%"></td>
   </tr>
   <tr align="center" valign="middle">
-    <td colspan="2">
+    <td colspan="3">
 	    <a href="javascript:void(0);" onClick="add_bbcode(\'body\',\'[b]\',\'[/b]\');"><img src="'.$theme_url.'/'.$settings['theme'].'/images/bbc_bold.png" alt="'.$l['forum_post_bold'].'" /></a>
 	    <a href="javascript:void(0);" onClick="add_bbcode(\'body\',\'[i]\',\'[/i]\');"><img src="'.$theme_url.'/'.$settings['theme'].'/images/bbc_italic.png" alt="'.$l['forum_post_italic'].'" /></a>
 	    <a href="javascript:void(0);" onClick="add_bbcode(\'body\',\'[u]\',\'[/u]\');"><img src="'.$theme_url.'/'.$settings['theme'].'/images/bbc_underline.png" alt="'.$l['forum_post_underline'].'" /></a>
@@ -28,7 +30,7 @@ echo '
 	  </td>	
   </tr>
   <tr align="center" valign="middle">
-  	<td colspan="2">  
+  	<td colspan="3">  
 	    ';
 	
 	include_once $theme_dir.'/'.$settings['theme'].'/emoticons/emoticons.php';
@@ -43,7 +45,7 @@ echo '
 	  </td>
   </tr>
   <tr align="center">
-    <td colspan="2"><textarea id="body" name="body" rows="12" cols="60" onclick="if(document.selection){this.selection = document.selection.createRange()}" onkeyup="if(document.selection){this.selection = document.selection.createRange()}" onchange="if(document.selection){this.selection = document.selection.createRange().duplicate()}" onfocus="if(document.selection){this.selection = document.selection.createRange().duplicate()}">', $settings['body'], '</textarea></td>
+    <td colspan="3"><textarea id="body" name="body" rows="12" cols="60" onclick="if(document.selection){this.selection = document.selection.createRange()}" onkeyup="if(document.selection){this.selection = document.selection.createRange()}" onchange="if(document.selection){this.selection = document.selection.createRange().duplicate()}" onfocus="if(document.selection){this.selection = document.selection.createRange().duplicate()}">', $settings['body'], '</textarea></td>
   </tr>';
   if(canforum('post_sticky', $settings['board']) || canforum('lock_topic', $settings['board'])) {
     if($settings['sticky'])
@@ -56,12 +58,15 @@ echo '
       $settings['locked'] = '';
     echo '
     <tr align="center">
-      <td>', canforum('post_sticky', $settings['board']) ? $l['topic_sticky'].' <input name="sticky" '. $settings['sticky']. ' type="checkbox" value="1"/>' : '', '</td><td>', canforum('lock_topic', $settings['board']) ? $l['topic_lock'].' <input name="locked" '. $settings['locked']. ' type="checkbox" value="1"/>' : '', '</td>
+      <td colspan="3">', canforum('post_sticky', $settings['board']) ? $l['topic_sticky'].' <input name="sticky" '. $settings['sticky']. ' type="checkbox" value="1"/>' : '', '</td>
+    </tr>
+    <tr align="center">
+      <td colspan="3">', canforum('lock_topic', $settings['board']) ? $l['topic_lock'].' <input name="locked" '. $settings['locked']. ' type="checkbox" value="1"/>' : '', '</td>
     </tr>';
   }
 echo '
   <tr align="center">
-    <td colspan="2"><input name="make_topic" type="submit" value="', $l['topic_topic_button'], '"/></td>
+    <td colspan="3"><input name="make_topic" type="submit" value="', $l['topic_topic_button'], '"/></td>
   </tr>
 </table>
 </form>
@@ -77,13 +82,15 @@ echo '
 <p><input type="hidden" name="edit" value="'.$settings['edit'].'" /></p>
 <table id="post" border="0px">
   <tr cellspacing="0px" cellpadding="0px">
-    <td class="title" colspan="2">', $l['post_postreply'], '</td>
+    <td class="title" colspan="3">', $l['post_postreply'], '</td>
   </tr>
   <tr align="center">
-    <td>', $l['topic_subject'], '</td><td><input name="subject" type="text" size="80" value="', $settings['subject'], '"/>
+    <td width="20%">', $l['topic_subject'], '</td>
+    <td><input name="subject" type="text" size="78" value="', $settings['subject'], '" />
+    <td width="20%"></td>
   </tr>
  <tr align="center" valign="middle">
-    <td colspan="2">
+    <td colspan="3">
 	    <a href="javascript:void(0);" onClick="add_bbcode(\'body\',\'[b]\',\'[/b]\');"><img src="'.$theme_url.'/'.$settings['theme'].'/images/bbc_bold.png" alt="'.$l['forum_post_bold'].'" /></a>
 	    <a href="javascript:void(0);" onClick="add_bbcode(\'body\',\'[i]\',\'[/i]\');"><img src="'.$theme_url.'/'.$settings['theme'].'/images/bbc_italic.png" alt="'.$l['forum_post_italic'].'" /></a>
 	    <a href="javascript:void(0);" onClick="add_bbcode(\'body\',\'[u]\',\'[/u]\');"><img src="'.$theme_url.'/'.$settings['theme'].'/images/bbc_underline.png" alt="'.$l['forum_post_underline'].'" /></a>
@@ -108,7 +115,7 @@ echo '
 	echo '</td>
   </tr>
   <tr align="center">
-    <td colspan="2"><textarea name="body" id="body" rows="12" cols="60"onclick="if(document.selection){this.selection = document.selection.createRange()}" onkeyup="if(document.selection){this.selection = document.selection.createRange()}" onchange="if(document.selection){this.selection = document.selection.createRange().duplicate()}" onfocus="if(document.selection){this.selection = document.selection.createRange().duplicate()}">', $settings['body'], '</textarea></td>
+    <td colspan="3"><textarea name="body" id="body" rows="12" cols="60"onclick="if(document.selection){this.selection = document.selection.createRange()}" onkeyup="if(document.selection){this.selection = document.selection.createRange()}" onchange="if(document.selection){this.selection = document.selection.createRange().duplicate()}" onfocus="if(document.selection){this.selection = document.selection.createRange().duplicate()}">', $settings['body'], '</textarea></td>
   </tr>';
   if(canforum('post_sticky', $settings['board']) || canforum('lock_topic', $settings['board'])) {
     if($settings['sticky'])
@@ -121,12 +128,15 @@ echo '
       $settings['locked'] = '';
     echo '
     <tr align="center">
-      <td>', canforum('post_sticky', $settings['board']) ? $l['topic_sticky'].' <input name="sticky" '. $settings['sticky']. ' type="checkbox" value="1"/>' : '', '</td><td>', canforum('lock_topic', $settings['board']) ? $l['topic_lock'].' <input name="locked" '. $settings['locked']. ' type="checkbox" value="1"/>' : '', '</td>
+      <td colspan="3">', canforum('post_sticky', $settings['board']) ? $l['topic_sticky'].' <input name="sticky" '. $settings['sticky']. ' type="checkbox" value="1"/>' : '', '</td>
+    </tr>
+    <tr align="center">
+      <td colspan="3">', canforum('lock_topic', $settings['board']) ? $l['topic_lock'].' <input name="locked" '. $settings['locked']. ' type="checkbox" value="1"/>' : '', '</td>
     </tr>';
   }
 echo '
   <tr align="center">
-    <td colspan="2"><input name="post_reply" type="submit" value="', $l['topic_post_button'] ,'"/></td>
+    <td colspan="3"><input name="post_reply" type="submit" value="', $l['topic_post_button'] ,'"/></td>
   </tr>
 </table>
 </form>
