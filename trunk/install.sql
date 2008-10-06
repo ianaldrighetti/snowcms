893c500c278780e65912458b3440cff46460446c
antimatter15 <antimatter15@f10a5a2d-b74f-0410-8a28-69e04d5e54c3>
2008-10-06 21:13:29 +0000

    allow guests to view general chat

--- trunk/install.sql
+++ trunk/install.sql
@@ -53,7 +53,7 @@ CREATE TABLE `{$db_prefix}boards` (
 ) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 INSERT INTO `{$db_prefix}boards` (`bid`,`cid`,`border`,`who_view`,`name`,`bdesc`,`numtopics`,`numposts`,`last_msg`,`last_uid`,`last_name`) VALUES
-('1','1','1','1,2','General Chat','Chat about anything!','0','0','0','0','0');
+('1','1','1','-1,2','General Chat','Chat about anything!','0','0','0','0','0');
 
 DROP TABLE IF EXISTS `{$db_prefix}categories`;
 
