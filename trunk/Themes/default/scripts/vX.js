0d3418ec582cc79fa3873252755e1e0ea0004cdd
antimatter15 <antimatter15@f10a5a2d-b74f-0410-8a28-69e04d5e54c3>
2008-10-12 01:27:02 +0000

    shaved off some bytes off vX by updating to newer version

--- trunk/Themes/default/scripts/vX.js
+++ trunk/Themes/default/scripts/vX.js
@@ -1,4 +1,1 @@
-/*vX Ajax Function. (C) Antimatter15 2008*/
-function vX(u,f,p){var x=(window.ActiveXObject)?new ActiveXObject("Microsoft.XMLHTTP"):new XMLHttpRequest();
-x.open(p?"POST":"GET",u,true);if(p) x.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-x.onreadystatechange=function(){if(x.readyState==4&&x.status==200) f(x.responseText)};x.send(p)}
+function vX(u,f,p,x){x=(window.ActiveXObject)?new ActiveXObject("Microsoft.XMLHTTP"):new XMLHttpRequest();x.open(p?"POST":"GET",u,true);p?x.setRequestHeader("Content-type","application/x-www-form-urlencoded"):p;x.onreadystatechange=function(){if(x.readyState==4&&x.status==200) f?f(x.responseText,x):f};x.send(p)}
