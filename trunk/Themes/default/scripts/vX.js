b5283cb748c4643c1ad28d1b28ab8d1ff53ab6dc
antimatter15 <antimatter15@f10a5a2d-b74f-0410-8a28-69e04d5e54c3>
2008-10-15 22:52:00 +0000

    292b vX!

--- trunk/Themes/default/scripts/vX.js
+++ trunk/Themes/default/scripts/vX.js
@@ -1,2 +1,1 @@
-vX=function(u,f,p,x){x=window.ActiveXObject?new ActiveXObject('Microsoft.XMLHTTP'):new XMLHttpRequest();x.open(p?'POST':'GET',u,!0);
-p?x.setRequestHeader('Content-type','application/x-www-form-urlencoded'):p;x.onreadystatechange=function(){x.readyState==4?f?f(x.responseText,x):f:0};x.send(p)}
+vX=function(u,f,p,x){x=window.ActiveXObject?new ActiveXObject('Microsoft.XMLHTTP'):new XMLHttpRequest();x.open(p?'POST':'GET',u,!0);p?x.setRequestHeader('Content-type','application/x-www-form-urlencoded'):0;x.onreadystatechange=function(){x.readyState==4?f?f(x.responseText,x):f:0};x.send(p)}
