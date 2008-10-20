d71c54a7f22c45dc0ed7e17d03545734fd9293f7
antimatter15 <antimatter15@f10a5a2d-b74f-0410-8a28-69e04d5e54c3>
2008-10-20 21:24:01 +0000

    bugfix

--- trunk/Themes/default/scripts/vX.js
+++ trunk/Themes/default/scripts/vX.js
@@ -9,7 +9,7 @@ _.F=function(d,h,f,i){d=d=='in';_.A(h,'opacity',d?0:1,d?1:0,'','',15,50);_.A(h,'
 _.G=function(e){return e.style?e:document.getElementById(e)}
 _.H=function(s,d){var t=document.createElement('textarea');t.innerHTML=s;return d?t.value:t.innerHTML}
 _.I=function(v,a){for(var i=a.length;i--&&a[i]!=v;);return i}
-_.N=function(n){var r=window,p=n.split('.');for(var i=0;i<p.length&&p[i];i++){if(!this.r[p[i]])r[p[i]]={};r=r[p[i]]}return r}
+_.N=function(n){var r=window,p=n.split('.');for(var i=0;i<p.length&&p[i];i++){if(r[p[i]]==undefined)r[p[i]]={};r=r[p[i]]}return r}
 _.Q=function(j,y,x){y='';for(x in j){y+='&'+x+'='+j[x]};return y.substr(1)}
 _.R=function(f){if(/(?!.*?ati|.*?kit)^moz|ope/i.test(navigator.userAgent)){_.E(document,'DOMContentLoaded',f)}else{setTimeout(f,0)}}
 _.S=function(j,d){if(d)return eval('('+j+')');if(!j)return j+'';var c=j.constructor+'',t=[];if(c.match(/arr/i)){for(x in j)t.push(_.S(j[x]));return'['+t.join(',')+']'}if(c.match(/obj/i)){for(x in j)t.push(x+':'+_.S(j[x]));return'{'+t.join(',')+'}'}if(c.match(/str/i))return"'"+j.replace(/\'/g,"\\'")+"'";return j}
