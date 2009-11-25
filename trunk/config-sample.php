167fe6fb41a0a62a2db615f73dfa6e657c6e7c4f
ian.aldrighetti <ian.aldrighetti@f10a5a2d-b74f-0410-8a28-69e04d5e54c3>
2009-11-25 05:06:08 +0000

    Look! SnowCMS v2.0's first commit :D Sweet huh?!?! Well, I doubt it runs, but the concept is there ;)

---
+++ trunk/config-sample.php
@@ -0,0 +1,51 @@
+<?php
+#########################################################################
+#                             SnowCMS v2.0                              #
+#                          By the SnowCMS Team                          #
+#                            www.snowcms.com                            #
+#                  Released under the GNU GPL v3 License                #
+#                     www.gnu.org/licenses/gpl-3.0.txt                  #
+#########################################################################
+#                                                                       #
+# SnowCMS originally pawned by soren121 started some time in early 2008 #
+#                                                                       #
+#########################################################################
+#                                                                       #
+#                SnowCMS v2.0 began in November 2009                    #
+#                                                                       #
+#########################################################################
+#                     File version: SnowCMS 2.0                         #
+#########################################################################
+
+# No direct access!!!
+if(!defined('IN_SNOW'))
+  die;
+
+#
+# config.php holds all your database information and paths.
+#
+
+# Database settings:
+$db_type = ''; # Your database type, an example would be mysql, sqlite or postgresql
+$db_host = ''; # The location of your database, could be localhost or a path (for SQLite)
+$db_user = ''; # The user that has access to your database, though not all database systems have this.
+$db_pass = ''; # The password to your database user.
+$db_name = ''; # The name of the database.
+$db_persist = false; # Whether or not to have a persistent connection to the database.
+$tbl_prefix = 'snow_'; # The prefix of the tables, allows multiple installs on the same database.
+
+# The location of your root directory of your SnowCMS installation.
+$base_dir = defined('__DIR__') ? __DIR__ : dirname(__FILE__);
+
+# Some other useful paths...
+$core_dir = $base_dir. '/core';
+$theme_dir = $base_dir. '/themes';
+$plugin_dir = $base_dir. '/plugins';
+
+# The address of where your SnowCMS install is accessible (No trailing /!)
+$base_url = '';
+$theme_url = $base_url. '/themes';
+
+# SnowCMS hasn't been installed yet, has it?
+$snowcms_installed = false;
+?>
---
+++ trunk/setup-mysql.sql
@@ -0,0 +1,55 @@
+----
+-- Holds members extra information, this is what a plugin should use to store extra member stuffs!
+----
+CREATE TABLE `{$db_prefix}member_data`
+(
+  `member_id` INT(11) UNSIGNED NOT NULL,
+  `variable` VARCHAR(255) NOT NULL,
+  `value` TEXT NOT NULL,
+  PRIMARY KEY (`member_id`, `variable`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+----
+--  The members table, holding, you guessed it! MEMBERS!
+---
+CREATE TABLE `{$db_prefix}members`
+(
+  `member_id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `member_name` VARCHAR(80) NOT NULL,
+  `member_pass` VARCHAR(40) NOT NULL,
+  `display_name` VARCHAR(255) NOT NULL,
+  `member_email` VARCHAR(100) NOT NULL,
+  `member_registered` INT(10) UNSIGNED NOT NULL,
+  `member_ip` VARCHAR(150) NOT NULL,
+  `member_activated` TINYINT(1) UNSIGNED NOT NULL DEFAULT '0',
+  `member_acode` VARCHAR(40) NOT NULL,
+  PRIMARY KEY (`member_id`),
+  KEY (`member_name`),
+  KEY (`display_name`),
+  KEY (`member_activated`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+----
+-- This is where currently enabled plugins are held
+----
+CREATE TABLE `{$db_prefix}plugins`
+(
+  `dependency_name` VARCHAR(255) NOT NULL,
+  `dependency_names` TEXT NOT NULL,
+  `dependencies` TINYINT(3) UNSIGNED NOT NULL,
+  `directory` VARCHAR(255) NOT NULL,
+  `runtime_error` TINYINT(1) UNSIGNED NOT NULL DEFAULT '0',
+  PRIMARY KEY (`dependency_name`),
+  KEY (`dependencies`),
+  KEY (`runtime_error`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+----
+-- A table holding various settings and what not xD
+----
+CREATE TABLE `{$db_prefix}settings`
+(
+  `variable` VARCHAR(255) NOT NULL,
+  `value` TEXT NOT NULL,
+  PRIMARY KEY (`variable`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
