d9abf0b3e0c59d24123b26608682e1bbc74847b6
Antimatter15 <Antimatter15@f10a5a2d-b74f-0410-8a28-69e04d5e54c3>
2010-01-17 17:37:35 +0000

    I fixed up JSON encoding using my code and anyway wasnt the plan to use jQuery instead

--- themes/default/js/snowobj.js
+++ themes/default/js/snowobj.js
@@ -71,34 +71,44 @@ function SnowObj()
     return Object.prototype.toString.call(checkObject) === '[object Array]' ? true : false;
   };
 
+  this.json_encode = function(obj){
+    //simple partial JSON encoder implementation
+    //http://gist.github.com/gists/240659 stolen from me
+    
+    if(window.JSON && JSON.stringify) return JSON.stringify(obj);
+    var enc = arguments.callee; //for purposes of recursion
+    
+    if(typeof obj == "boolean" || typeof obj == "number"){
+        return obj+'' //should work...
+    }else if(typeof obj == "string"){
+      //a large portion of this is stolen from Douglas Crockford's json2.js
+      return '"'+
+            obj.replace(
+              /[\\\"\x00-\x1f\x7f-\x9f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g
+            , function (a) {
+              return '\\u' + ('0000' + a.charCodeAt(0).toString(16)).slice(-4);
+            })
+            +'"'; //note that this isn't quite as purtyful as the usualness
+    }else if(this.is_array(obj)){
+      for(var i = 0; i < obj.length; i++){
+        obj[i] = enc(obj[i]); //encode every sub-thingy on top
+      }
+      return "["+obj.join(",")+"]";
+    }else{
+      var pairs = []; //pairs will be stored here
+      for(var k in obj){ //loop through thingys
+        pairs.push(enc(k)+":"+enc(obj[k])); //key: value
+      }
+      return "{"+pairs.join(",")+"}" //wrap in the braces
+    }
+  }
+
   this.json = function(data, serialize)
   {
     if(serialize)
     {
-      // Number..? Its just that ;)
-      if(typeof data == 'number')
-        return data;
-      else if(typeof data == 'string')
-        return '"' + data + '"';
-      else if(typeof data == 'object')
-      {
-        var serializedString = '';
-        var values = new Array();
-
-        // idk how to do this!!!
-        if(this.is_array(data))
-        {
-
-        }
-        else
-        {
-        }
-
-        return '{' + serializedString + '}';
-      }
-    }
-    else
-      return eval('(' + data + ')');
+      return this.json_encode(data)
+    }else return eval('(' + data + ')');
   }
 
   this.screenheight = function()
