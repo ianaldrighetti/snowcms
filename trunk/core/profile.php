8234994faef449e285584b8f5854ab50054b3ee4
ian.aldrighetti <ian.aldrighetti@f10a5a2d-b74f-0410-8a28-69e04d5e54c3>
2010-05-09 04:42:41 +0000

    Started on member profiles, also implemented menu displaying in the default theme.

--- trunk/core/profile.php
+++ trunk/core/profile.php
@@ -21,4 +21,104 @@ if(!defined('IN_SNOW'))
   die;
 
 # Title: Profile
+
+if(!function_exists('profile_view'))
+{
+  /*
+    Function: profile_view
+
+    Handles the viewing of members profiles, but also editing of their
+    profiles as well.
+
+    Parameters:
+      none
+
+    Returns:
+      void - Nothing is returned by this function.
+
+    Note:
+      This function is overloadable.
+  */
+  function profile_view()
+  {
+    global $api, $member, $settings;
+
+    $api->run_hooks('profile_view');
+
+    # Are you viewing someone elses profile? If it's yours, ignore it.
+    if(isset($_GET['id']) && $_GET['id'] != $member->id())
+    {
+      # Can you even view other profiles?
+      if(!$member->can('view_other_profile'))
+      {
+        # Nope, you cannot.
+        member_access_denied(null, l('Sorry, but you do not have permission to view other members profiles.'));
+      }
+      else
+      {
+        # Does the member even exist..? If not, then we will show a denied page.
+        $members = $api->load_class('Members');
+        $members->load($_GET['id']);
+
+        $member_info = $members->get($_GET['id']);
+
+        # So did it load?
+        if($member_info == false)
+        {
+          # No it did not. So the member doesn't exist :-(
+          member_access_denied(l('Member doesn\'t exist'), l('Sorry, but the member you are requesting does not exist.'));
+        }
+      }
+    }
+  }
+}
+
+if(!function_exists('member_access_denied'))
+{
+  /*
+    Function: member_access_denied
+
+    Shows an error screen denying the member access to the page they requested.
+
+    Parameters:
+      string $title - The title of the page, defaults to Access denied
+      string $message - The error message to display, defaults to "Sorry,
+                        but you are not allowed to access the page you have requested."
+
+    Returns:
+      void - Nothing is returned by this function.
+
+    Note:
+      This function is overloadable.
+  */
+  function member_access_denied($title = null, $message = null)
+  {
+    global $theme;
+
+    if(empty($title))
+    {
+      $title = l('Access denied');
+    }
+
+    if(empty($message))
+    {
+      $message = l('Sorry, but you are not allowed to access the page you have requested.');
+    }
+
+    $theme->set_title($title);
+
+    $theme->add_meta(array('name' => 'robots', 'content' => 'noindex'));
+
+    $theme->header();
+
+    echo '
+    <h1>', $title, '</h1>
+    <p>', $message, '</p>';
+
+    $theme->footer();
+
+    # Exit!
+    exit;
+  }
+}
 ?>
