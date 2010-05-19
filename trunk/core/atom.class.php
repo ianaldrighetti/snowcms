b41ad63101f7681665970c7dd813725869699499
ian.aldrighetti@gmail.com <ian.aldrighetti@gmail.com@f10a5a2d-b74f-0410-8a28-69e04d5e54c3>
2010-05-19 15:57:24 +0000

    The Update class is done, though not yet tested, but once tested, will be implemented in the system update of the control panel (Fixes issue #42).
    Created the initial files for the Atom and RSS classes, regarding issue #55.

---
+++ trunk/core/atom.class.php
@@ -0,0 +1,34 @@
+<?php
+#########################################################################
+#                             SnowCMS v2.0                              #
+#                          By the SnowCMS Team                          #
+#                            www.snowcms.com                            #
+#                  Released under the GNU GPL v3 License                #
+#                     www.gnu.org/licenses/gpl-3.0.txt                  #
+#########################################################################
+#                                                                       #
+# SnowCMS originally pawned by soren121 started some time in early 2008 #
+#                                                                       #
+#########################################################################
+#                                                                       #
+#                SnowCMS v2.0 began in November 2009                    #
+#                                                                       #
+#########################################################################
+#                     File version: SnowCMS 2.0                         #
+#########################################################################
+
+if(!defined('IN_SNOW'))
+  die;
+
+/*
+  Class: Atom
+
+  In order to allow plugins and others to quickly create Atom feeds without
+  too much hassle, the Atom class allows you to pass data to the class and
+  then generate an Atom feed. Also see <RSS>.
+*/
+class Atom
+{
+
+}
+?>
