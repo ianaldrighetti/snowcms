674dd2f2ad9d59d313b8a106cf16b4c30f334548
doodle62 <doodle62@f10a5a2d-b74f-0410-8a28-69e04d5e54c3>
2011-07-08 17:13:22 +0000

    New default theme! :D

--- trunk/themes/default/header.template.php
+++ trunk/themes/default/header.template.php
@@ -3,23 +3,17 @@ if(!defined('INSNOW'))
 {
   die('Nice try...');
 }
-?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-  <title><?php theme_title(); ?></title>
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+	<title><?php theme_title(); ?></title>
 	<?php theme_head(); ?>
 </head>
+
 <body>
-  <div id="wrapper">
-    <div id="header">
-      <h2><?php theme_site_name(); ?></h2>
-      <p><?php theme_sub_title(); ?></p>
-      <div id="menu-outer">
-        <div id="menu-inner">
-          <ul id="menu">
-<?php theme_menu(); ?>
-          </ul>
-        </div>
-      </div>
-    </div>
-    <div id="content">
+	<div id="header"></div>
+	<div id="wrapper">
+		<h2><?php theme_site_name(); ?></h2>
+		<div id="content">
