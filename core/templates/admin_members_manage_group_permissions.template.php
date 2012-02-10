7511e5aa1d42fdec18f014b80774b099b3d95ded
ian.aldrighetti <ian.aldrighetti@f10a5a2d-b74f-0410-8a28-69e04d5e54c3>
2012-02-10 04:42:32 +0000

    ! The page displaying current group permissions has been updated to categorize permissions making it easier to find what you're looking for (fixes issue #93). ! I have changed how the system counts the number of permissions a group has been denied, disallowed and/or allowed, as it does it all at once instead of group by group.

--- core/templates/admin_members_manage_group_permissions.template.php
+++ core/templates/admin_members_manage_group_permissions.template.php
@@ -36,5 +36,44 @@ if(!defined('INSNOW'))
 		<h3><img src="', theme()->url(), '/style/images/members_permissions-small.png" alt="" /> ', l('Managing %s Permissions', api()->context['group_name']), '</h3>
 		<p>', l('Select the permissions you would like to assign to the member group below. <em>Deny</em> means the permission will be denied for the member even if another group they are assigned to allows the permission. <em>Disallow</em> means the permission is not allowed by members of the group, unless another group they are assigned to allows that permission.'), '</p>';
 
-			api()->context['form']->render(api()->context['group_id']. '_permissions');
+if(!empty(api()->context['message']))
+{
+	echo '
+		<div class="message-box">
+			<p>', api()->context['message'], '</p>
+		</div>';
+}
+
+echo '
+		', api()->context['form']->open(api()->context['group_id']. '_permissions');
+
+// Since we aren't using the built-in method to render the form, we will
+// have to do it ourself, but that's the price of having a custom layout.
+foreach(api()->context['permissions'] as $permission)
+{
+	echo '
+		<div class="permission-table">
+			<p class="permission-header">', htmlchars($permission['label']), (!empty($permission['subtext']) ? ' <span class="permission-subtext">'. htmlchars($permission['subtext']). '</span>' : ''), '</p>';
+
+	foreach($permission['permissions'] as $perm)
+	{
+		echo '
+			<div class="float-left">
+				<p class="permission-label">', htmlchars($perm['label']), '</p>', !empty($perm['subtext']) ? '
+				<p class="permission-label-subtext">'. htmlchars($perm['subtext']). '</p>' : '', '
+			</div>
+			<div class="float-right">
+				<p>', api()->context['form']->input($perm['id'])->generate(), '</p>
+			</div>
+			<div class="break">
+			</div>';
+	}
+
+	echo '
+		</div>';
+}
+
+echo '
+			<p class="center"><input type="submit" name="', api()->context['group_id'], '_permissions" value="', l('Update'), '" /></p>
+		', api()->context['form']->close(api()->context['group_id']. '_permissions');
 ?>
